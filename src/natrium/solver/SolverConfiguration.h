2ab5000cbd414ee84331f84d1bf32cd6ff0c13a0
kraemer <andreas.kraemer@h-brs.de>
2014-05-19 14:46:49 +0200

    SolverConfiguration file divided in .h and .cpp

--- src/natrium/solver/SolverConfiguration.h
+++ src/natrium/solver/SolverConfiguration.h
@@ -78,6 +78,9 @@ public:
 	ConfigurationException(const char *msg) :
 			message(msg) {
 	}
+	ConfigurationException(const string& msg) :
+			message(msg) {
+	}
 	~ConfigurationException() throw () {
 	}
 	const char *what() const throw () {
@@ -142,136 +145,33 @@ private:
 
 public:
 
-	/// constructor
-	SolverConfiguration() {
-		// Declare structure of the parameter file
-		enter_subsection("General");
-		{
-			declare_entry("Time step size", "0.2",
-					dealii::Patterns::Double(1e-10),
-					"Size of the (initial) time step.");
-			declare_entry("Switch output off?", "false",
-					dealii::Patterns::Bool(), "Switch output off, completely.");
-			declare_entry("Stencil", "D2Q9",
-					dealii::Patterns::Selection("D2Q9"),
-					"The discrete velocity stencil. The number behind D denotes the dimension (2 or 3). The number behind Q denotes the number of particle directions in the discrete velocity model.");
-			declare_entry("Stencil scaling", "1.0",
-					dealii::Patterns::Double(1e-10),
-					"The scaling of the discrete velocities. Whereas in the standard LBM the magnitude of the particle velocities is set to 1.0 due to the uniform mesh grid, the SEDG-LBM features scaled particle velocities. As the scaling factor is proportional to the speed of sound, it strongly impacts the relaxation time.");
-		}
-		leave_subsection();
-		enter_subsection("Advection");
-		{
-			declare_entry("Advection scheme", "SEDG",
-					dealii::Patterns::Selection("SEDG"),
-					"The algorithm which is used for the advection (=streaming) step. While the LBM on a uniform mesh facilitates streaming towards a simple index shift, non-uniform meshes need a more sophisticated advection scheme.");
-			declare_entry("Time integrator", "5-stage Runge Kutta",
-					dealii::Patterns::Selection("5-stage Runge Kutta"),
-					"The algorithm which is used for the time integration of the discretizted advection (=streaming) equation. A time integrator is required, when the advection scheme is based upon some Finite Element/Difference/Volume or discontinuous Galerkin scheme.");
+	/**
+	 * @short Constructor -- initializes parameters with their default values
+	 */
+	SolverConfiguration();
 
-			enter_subsection("SEDG");
-			{
-				declare_entry("Order of finite element", "4",
-						dealii::Patterns::Integer(2),
-						"The degree of the polynomial shape functions used by the SEDG scheme.");
-				declare_entry("Flux type", "Lax-Friedrichs",
-						dealii::Patterns::Selection("Lax-Friedrichs|Central"),
-						"The flux connects the shape functions between neighboring elements. It is strongly recommended to use the Lax-Friedrichs scheme which is a forward-discretization along characteristics, rather than a central flux.");
-			}
-			leave_subsection();
-		}
-		leave_subsection();
-		enter_subsection("Collision");
-		{
-			declare_entry("Collision scheme",
-					"BGK with transformed distribution functions",
-					dealii::Patterns::Selection(
-							"BGK with transformed distribution functions"),
-					"The collision step models velocity changes due to particle collisions (local at each node) by a relaxation towards thermodynamic equilibrium. There are several approaches, e.g. the single-relaxation time Bhatnagar-Groß-Krook. Using transformed particle distribution functions enhances the accuracy of the LBM.");
-			declare_entry("Collision on boundary nodes", "true",
-					dealii::Patterns::Bool(),
-					"States whether the collision step is to be done on all nodes or only on internal nodes. E.g. the standard bounce back scheme is of 2nd order, when collisions take place at boundary nodes, and of 1st order, if not.");
-
-		}
-		leave_subsection();
-
-		enter_subsection("Initialization");
-		{
-			declare_entry("Restart at last checkpoint?", "true",
-					dealii::Patterns::Bool(),
-					"The solver can be restarted at the last stored checkpoint, in case that an old run had been aborted at some point of time.");
-			declare_entry("Initialization scheme", "Equilibrium",
-					dealii::Patterns::Selection("Equilibrium|Iterative"),
-					"The initial particle distribution functions are normally assumed to be in local equilibrium. A more stable (and costly) scheme is to do some streaming steps on the density field but not on the velocity field, before starting the actual simulations (see e.g. the Book of Guo and Shu).");
-			enter_subsection("Iterative Initialization stop condition");
-			{
-				declare_entry("Residual", "1e-6",
-													dealii::Patterns::Double(1e-25),
-													"The iterative initialization stops, when the density increment is smaller than the residual, i.e. the iteration has converged.");
-				declare_entry("Number of iterations", "2000",
-													dealii::Patterns::Integer(1),
-													"The iterative initialization stops at the latest after a specific number of iterations.");
-
-			}
-			leave_subsection();
-		}
-		leave_subsection();
-
-		enter_subsection("Stop condition");
-		{
-			declare_entry("Number of iterations", "1000000",
-					dealii::Patterns::Integer(1),
-					"The maximum number of iterations.");
-
-		}
-		leave_subsection();
-
-		enter_subsection("Output");
-		{
-			declare_entry("Output directory", "/tmp/NATriuM",
-					dealii::Patterns::DirectoryName(),
-					"The name of the directory to which the output is written.");
-			declare_entry("Output checkpoint interval", "1000",
-					dealii::Patterns::Integer(1),
-					"Write out checkpoint files every ... step.");
-			declare_entry("Output solution interval", "1000",
-					dealii::Patterns::Integer(1),
-					"Write out solution every ... step.");
-			declare_entry("Command line verbosity", "Basic",
-					dealii::Patterns::Selection("Error|Basic|Full"),
-					"The amount of command line output.");
-			declare_entry("Write a log file?", "true", dealii::Patterns::Bool(),
-					"Specifies if log is written to a file.");
-		}
-		leave_subsection();
-
-
-		// TODO read configuration from file
-		// TODO custom configurations
-		m_advectionOperatorType = Advection_SEDGMinLee;
-		m_collisionType = Collision_BGKTransformed;
-		m_stencilType = Stencil_D2Q9;
-		m_timeIntegratorType = Integrator_RungeKutta5LowStorage;
-		m_fluxType = Flux_LaxFriedrichs;
-		m_timeStep = 0.1;
-		m_orderOfFiniteElement = 2;
-		m_numberOfTimeSteps = 100;
-		m_dQScaling = 1.0;
-		m_outputDirectory = "/tmp/natrium";
-		setOutputFlags(out_CommandLineBasic | out_VectorFields);
-		m_restart = false;
-		m_distributionInitType = Equilibrium;
-		m_maxDistributionInitIterations = 10000;
-		m_stopDistributionInitResidual = 1e-6;
-		m_outputVectorFieldsEvery = 10;
-		m_outputCheckpointEvery = 500;
-	}
-	;
+	/**
+	 * @short Constructor -- initializes parameters from an xmlfile
+	 * @throws ConfigurationException if the XML file has wrong format
+	 */
+	SolverConfiguration(const std::string& XMLfilename);
 
 	/// destructor
 	virtual ~SolverConfiguration() {
 	}
 	;
+
+
+	/**
+	 * @short wrapper function for ParameterHandler::read_input; directing cerr into a C++-Exception
+	 **/
+	void readFromTextFile(const std::string & filename, const bool optional =
+			false, const bool write_stripped_file = false);
+
+	/**
+	 * @short wrapper function for ParameterHandler::read_input_from_xml; directing cerr into a C++-Exception
+	 **/
+	void readFromXMLFile(const std::string & filename);
 
 	/**
 	 * @short Check if the configuration is consistent
--- src/test/solver/SolverConfiguration_test.cpp
+++ src/test/solver/SolverConfiguration_test.cpp
@@ -23,14 +23,52 @@ BOOST_AUTO_TEST_CASE(CFDSolverConfiguration_Construction_test){
 	cout << "done" << endl;
 }
 
-BOOST_AUTO_TEST_CASE(CFDSolverConfiguration_CreateXMLFile_test){
+BOOST_AUTO_TEST_CASE(CFDSolverConfiguration_CreateParameterFiles_test){
 	cout << "CFDSolverConfiguration_CreateXMLFile_test..." << endl;
 	SolverConfiguration config;
 
-	std::ofstream paraOutFile("../results/NATriuM_parameters.xml");
-	config.print_parameters(paraOutFile, dealii::ParameterHandler::XML);
+	std::ofstream paraOutFile1("../results/NATriuM_parameters.xml");
+	config.print_parameters(paraOutFile1, dealii::ParameterHandler::XML);
+
+	std::ofstream paraOutFile2("../results/NATriuM_parameters.tex");
+	config.print_parameters(paraOutFile2, dealii::ParameterHandler::LaTeX);
+
+	std::ofstream paraOutFile3("../results/NATriuM_parameters.txt");
+	config.print_parameters(paraOutFile3, dealii::ParameterHandler::Description);
+
+
 	cout << "done" << endl;
 }
+
+BOOST_AUTO_TEST_CASE(CFDSolverConfiguration_CheckSet_test){
+	cout << "CFDSolverConfiguration_CheckSet_test..." << endl;
+	SolverConfiguration config;
+
+	// Set parameter
+	config.enter_subsection("Initialization");
+	config.set("Initialization scheme", "Iterative");
+	BOOST_CHECK(config.get("Initialization scheme") == "Iterative");
+	BOOST_CHECK_THROW(config.set("Initialization scheme", "guppy guppy"), std::exception );
+
+	// The same for pointers
+	shared_ptr<SolverConfiguration> config2 = make_shared<SolverConfiguration>();
+	config2->enter_subsection("Initialization");
+	config2->set("Initialization scheme", "Iterative");
+	BOOST_CHECK(config2->get("Initialization scheme") == "Iterative");
+	BOOST_CHECK_THROW(config2->set("Initialization scheme", "guppy guppy"), std::exception );
+
+	cout << "done" << endl;
+} /*CFDSolverConfiguration_CheckSet_test*/
+
+BOOST_AUTO_TEST_CASE(CFDSolverConfiguration_ReadFromFile_test){
+	cout << "CFDSolverConfiguration_ReadFromFile_test..." << endl;
+	SolverConfiguration config;
+
+	config.readFromXMLFile("../src/preprocessing/NATriuM_parameters.xml");
+	BOOST_CHECK_THROW(config.readFromXMLFile("../src/test/solver/invalid_parameters.xml"), ConfigurationException);
+
+	cout << "done" << endl;
+} /*CFDSolverConfiguration_ReadFromFile_test*/
 
 
 BOOST_AUTO_TEST_CASE(CFDSolverConfiguration_OutputFlags_test){
