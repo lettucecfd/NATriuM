21652bbc948c7ded9fd6b63e387010387c0c2dd9
Andreas <andreas.kraemer@h-brs.de>
2014-06-05 14:02:43 +0000

    First convergence analysis for the Taylor-Green vortex. (works for basic scaling (=linear;Chapman-Enskog); doesn't work for diffusive scaling (Junk et. al.), yet)
    Former-commit-id: feb8bd8548187666db9ce9a2da2f681c01160017

---
+++ src/analysis/valgrind-analysis/cg.sh
@@ -0,0 +1,1 @@
+valgrind --tool=callgrind -v --log-file=cg.log ../../../bin/analysis/convergence-analysis-basic/convergence-analysis-basic
---
+++ src/analysis/valgrind-analysis/vg.sh
@@ -0,0 +1,2 @@
+clear
+valgrind --leak-check=yes --track-origins=yes --track-fds=yes --log-file=vg.log ../../../bin/analysis/convergence-analysis-basic/convergence-analysis-basic
