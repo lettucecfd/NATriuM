aaf335ae011bde689d65904c873c4f68c7afd3eb
Dominik Wilde <dominik.wilde@gmail.com>
2015-04-15 22:35:14 +0200

    ExpTimeIntegrator improved
    Former-commit-id: 797cb20d3c7da1c2d234ccdd4e5650bd1a355362 [formerly 56a1d5822c8275fbc4d9a7edbd6617216338352a]
    Former-commit-id: cb2c78f408bd9a13d16e7f197c0fbb15cf44068c

--- src/natrium/timeintegration/ExponentialTimeIntegrator.cpp
+++ src/natrium/timeintegration/ExponentialTimeIntegrator.cpp
@@ -16,9 +16,10 @@ template<> ExponentialTimeIntegrator<distributed_sparse_matrix,
 		distributed_vector>::ExponentialTimeIntegrator(double timeStepSize,
 		size_t problemSize) :
 		TimeIntegrator<distributed_sparse_matrix, distributed_vector>(
-				timeStepSize), identityMatrix(makeIdentityMatrix()), H_m(
-				makeMatrix(arnoldiSize, arnoldiSize)), H(
-				makeMatrix(arnoldiSize + 2, arnoldiSize + 2)) {
+				timeStepSize), m_identityMatrix(makeIdentityMatrix()), m_Hm(
+				makeMatrix(arnoldiSize, arnoldiSize)), m_H(
+				makeMatrix(arnoldiSize + 2, arnoldiSize + 2)), m_phiOne(makeMatrix(arnoldiSize, arnoldiSize)),
+				m_phiTwo(makeMatrix(arnoldiSize, arnoldiSize)), m_phiExtended(makeMatrix(arnoldiSize + 1,arnoldiSize + 1)) {
 
 }
 
@@ -26,10 +27,12 @@ template<> ExponentialTimeIntegrator<distributed_sparse_block_matrix,
 		distributed_block_vector>::ExponentialTimeIntegrator(
 		double timeStepSize, size_t problemSize, size_t numberOfBlocks) :
 		TimeIntegrator<distributed_sparse_block_matrix, distributed_block_vector>(
-				timeStepSize), identityMatrix(makeIdentityMatrix()), H_m(
-				makeMatrix(arnoldiSize, arnoldiSize)), H(
-				makeMatrix(arnoldiSize + 2, arnoldiSize + 2)) {
+				timeStepSize), m_identityMatrix(makeIdentityMatrix()), m_Hm(
+				makeMatrix(arnoldiSize, arnoldiSize)), m_H(
+				makeMatrix(arnoldiSize + 2, arnoldiSize + 2)), m_phiOne(makeMatrix(arnoldiSize, arnoldiSize)),
+				m_phiTwo(makeMatrix(arnoldiSize, arnoldiSize)), m_phiExtended(makeMatrix(arnoldiSize + 1,arnoldiSize + 1)) {
 }
+
 
 template<class MATRIX, class VECTOR> double ExponentialTimeIntegrator<MATRIX,
 		VECTOR>::step(VECTOR& f, const MATRIX& systemMatrix,
@@ -46,114 +49,122 @@ template<class MATRIX, class VECTOR> double ExponentialTimeIntegrator<MATRIX,
 		dt = this->getTimeStepSize();
 	}
 
-	if (w.size() != f.size()) {
-		w.reinit(f.size(), true);
+	if (m_w.size() != f.size()) {
+		m_w.reinit(f.size(), true);
 	}
 
-	if (v_j.size() != f.size()) {
-		v_j.reinit(f.size(), true);
+	if (m_vj.size() != f.size()) {
+		m_vj.reinit(f.size(), true);
 	}
 
-	if (v_i.size() != f.size()) {
-		v_i.reinit(f.size(), true);
+	if (m_vi.size() != f.size()) {
+		m_vi.reinit(f.size(), true);
 	}
 
-	if (firstColumn.size() != arnoldiSize + 1) {
-		firstColumn.reinit(arnoldiSize + 1, true);
+	if (m_firstColumn.size() != arnoldiSize + 1) {
+		m_firstColumn.reinit(arnoldiSize + 1, true);
 	}
 
 	if (m_f.size() != f.size()) {
 		m_f.reinit(f.size(), true);
 	}
 
-	w = f;
-	v_j = f;
-	v_i = f;
+	m_w = f;
+	m_vj = f;
+	m_vi = f;
 
-	for (int i = 0; i < arnoldiSize + 2; i++) {
-		for (int j = 0; j < arnoldiSize + 2; j++) {
-			H(i, j) = 0;
+	for (int i = 0; i < arnoldiSize + 2; i++)
+	{
+		for (int j = 0; j < arnoldiSize + 2; j++)
+		{
+			m_H(i, j) = 0;
 		}
 	}
 
 	numeric_matrix V(f.size(), arnoldiSize + 1); // Orthonormal basis V_(m+1)
 
-	systemMatrix.vmult(w, f); 	// w = A*f + u
-	w.add(systemVector);		// w = A*f + u
+	systemMatrix.vmult(m_w, f); 	// w = A*f + u
+	m_w.add(systemVector);		// w = A*f + u
 
-	double beta = w.l2_norm();
+	double beta = m_w.l2_norm();
 
 	for (int i = 0; i < f.size(); i++) 		// Arnoldi algorithm (first step)
 
-			{
-		V.set(i, 0, w(i) / beta);
-
+	{
+		V.set(i, 0, m_w(i) / beta);
 	}
 
 	for (int j = 0; j < arnoldiSize; j++)  	// Arnoldi algorithm (second step)
-			{
-
-		for (int i = 0; i < f.size(); i++) {
-			v_j(i) = V(i, j);
+	{
+		for (int i = 0; i < f.size(); i++)
+		{
+			m_vj(i) = V(i, j);
 		}
 
-		systemMatrix.vmult(w, v_j);
+		systemMatrix.vmult(m_w, m_vj);
 
-		for (int i = 0; i <= j; i++) {
-			for (int k = 0; k < f.size(); k++) {
-				v_i(k) = V(k, i);
+		for (int i = 0; i <= j; i++)
+		{
+			for (int k = 0; k < f.size(); k++)
+			{
+				m_vi(k) = V(k, i);
 			}
 
-			H(i, j) = w * v_i;
-			v_i *= H(i, j);
-			w -= v_i;
+			m_H(i, j) = m_w * m_vi;
+			m_vi *= m_H(i, j);
+			m_w -= m_vi;
 
 		}
 
-		H(j + 1, j) = w.l2_norm();
-		if (H(j + 1, j) != 0) // && j<arnoldiSize-1)
+		m_H(j + 1, j) = m_w.l2_norm();
+		if (m_H(j + 1, j) != 0) // && j<arnoldiSize-1)
+		{
+			for (int k = 0; k < f.size(); k++)
 				{
-			for (int k = 0; k < f.size(); k++) {
-				V(k, j + 1) = w(k) / H(j + 1, j);
-
-			}
+					V(k, j + 1) = m_w(k) / m_H(j + 1, j);
+				}
 		}
-		H(arnoldiSize + 1, arnoldiSize) = 1;
+		m_H(arnoldiSize + 1, arnoldiSize) = 1;
 
 	}
 
-	for (int i = 0; i < arnoldiSize; i++) // Transform H to H_m
-		for (int j = 0; j < arnoldiSize; j++) {
-			{
-				H_m(i, j) = H(i, j);
-			}
+	for (int i = 0; i < arnoldiSize; i++)	// Transform H to Hm
+	{
+		for (int j = 0; j < arnoldiSize; j++)
+		{
+			m_Hm(i, j) = m_H(i, j);
 		}
+	}
 
-	numeric_matrix phiOne(arnoldiSize);
+/*	numeric_matrix phiOne(arnoldiSize);
 	numeric_matrix phiTwo(arnoldiSize);
-	numeric_matrix phiExtended(arnoldiSize + 1);
+	numeric_matrix phiExtended;*/
 
-	phiFunction(H_m, phiOne, phiTwo);
+	phiFunction(m_Hm, m_phiOne, m_phiTwo);
 
-	for (int i = 0; i < arnoldiSize; i++) {
-		for (int j = 0; j < arnoldiSize; j++) {
-			phiExtended(i, j) = phiOne(i, j);
+	for (int i = 0; i < arnoldiSize; i++)
+	{
+		for (int j = 0; j < arnoldiSize; j++)
+		{
+			m_phiExtended(i, j) = m_phiOne(i, j);
 		}
-		phiExtended(i, arnoldiSize) = 0;
+		m_phiExtended(i, arnoldiSize) = 0;
 	}
 
-	for (int j = 0; j < arnoldiSize; j++) {
-		phiExtended(arnoldiSize, j) = H(arnoldiSize, arnoldiSize - 1)
+	for (int j = 0; j < arnoldiSize; j++)
+	{
+		m_phiExtended(arnoldiSize, j) = m_H(arnoldiSize, arnoldiSize - 1)
 				* this->getTimeStepSize();
-		phiExtended(arnoldiSize, j) *= phiTwo(arnoldiSize - 1, j);
+		m_phiExtended(arnoldiSize, j) *= m_phiTwo(arnoldiSize - 1, j);
 	}
 
-	phiExtended(arnoldiSize, arnoldiSize) = 1;
+	m_phiExtended(arnoldiSize, arnoldiSize) = 1;
 
-	for (int i = 0; i < arnoldiSize + 1; i++) {
-		firstColumn(i) = phiExtended(i, 0);
+	for (int i = 0; i < arnoldiSize + 1; i++)
+	{
+		m_firstColumn(i) = m_phiExtended(i, 0);
 	}
-	V.vmult(m_f, firstColumn);
+	V.vmult(m_f, m_firstColumn);
 
 	m_f *= (dt * beta);
 
@@ -163,14 +174,156 @@ template<class MATRIX, class VECTOR> double ExponentialTimeIntegrator<MATRIX,
 		f(i) += m_f(i);
 	}
 
-	/*V.vmult(m_f,firstColumn);
+	/*V.vmult(m_f,m_firstColumn);
 	 m_f*=beta;
 	 f = m_f;
 	 */
 
 	return t + dt;
 
+
+
 }
+
+template<class MATRIX, class VECTOR> double ExponentialTimeIntegrator<MATRIX,VECTOR>::factorial(int base)
+{
+	unsigned int factorial =1;
+	for (int i=1;i<=base;i++)
+	{
+		factorial = factorial * i;
+	}
+	return factorial;
+}
+
+template<class MATRIX, class VECTOR> numeric_matrix ExponentialTimeIntegrator<MATRIX,VECTOR>:: makeIdentityMatrix()
+	{
+		numeric_matrix identityM(arnoldiSize);
+		for (int i=0;i<arnoldiSize;i++)
+			for(int j=0;j<arnoldiSize;j++)
+			{
+				{
+					if(i==j)
+						identityM(i,j)=1;
+					else
+						identityM(i,j)=0 ;
+				}
+			}
+
+		return identityM;
+	}
+
+template<class MATRIX, class VECTOR> numeric_matrix ExponentialTimeIntegrator<MATRIX,VECTOR>::makeMatrix(size_t m, size_t n)
+	{
+		numeric_matrix newMatrix(m,n);
+		return newMatrix;
+	}
+
+// Calculates the matrix exponential of the given matrix;
+template<class MATRIX, class VECTOR> numeric_matrix ExponentialTimeIntegrator<MATRIX,VECTOR>::taylorSeries(numeric_matrix base)
+	{
+		numeric_matrix exponential(arnoldiSize);  // Builds the identity matrix
+		numeric_matrix factor(arnoldiSize);
+		exponential.copy_from(m_identityMatrix);
+
+		factor = base;
+		exponential.add(factor,1);
+
+		for(int j=2;j<taylorSteps;j++) // Taylor series as matrix exponential
+		{
+			factor.mmult(factor,base);
+			factor /=j;
+			exponential.add (factor, 1);
+		}
+		return exponential;
+	}
+
+template<class MATRIX, class VECTOR> numeric_matrix ExponentialTimeIntegrator<MATRIX,VECTOR>::padeApproximation (numeric_matrix base)
+	{
+		numeric_matrix exponential(arnoldiSize);  // Builds the identity matrix
+		numeric_matrix factor(arnoldiSize);
+		numeric_matrix aid(arnoldiSize);
+		numeric_matrix N(arnoldiSize);
+		numeric_matrix D(arnoldiSize);
+		int m=taylorSteps;
+
+		double s = base.linfty_norm();
+
+		if (s > 0.5)
+		{
+			s = trunc(log(s)/log(2))+2;
+			if (s<0)
+				{
+				s=0;
+				}
+			 base *= (pow(2,(-s)));
+		}
+
+		double fact = 1.0;
+
+		factor.copy_from(m_identityMatrix);
+		N=factor;
+		D=factor;
+
+		for(int n=1;n<=m;n++) // Padé approximation as matrix exponential
+		{
+			double a = m;
+			double b = n;
+
+			fact = factorial(2*a-b)*factorial(a)/(factorial(2*a)*factorial(b)*factorial(a-b));
+
+			factor.mmult(aid,base);
+			factor = aid;
+
+			aid *= fact;
+			N.add(aid,1);
+
+			if(n%2!=0)
+			{
+				aid *= (-1);
+			}
+
+			D.add(aid,1);
+
+						}
+			D.gauss_jordan();
+
+			D.mmult(exponential,N);
+
+			for (int i=1;i<=s;i++)
+			{
+				D=exponential;
+				D.mmult(N,exponential);
+				exponential = N;
+			}
+
+			return exponential;
+
+	}
+
+template<class MATRIX, class VECTOR> void ExponentialTimeIntegrator<MATRIX,VECTOR>:: phiFunction(const numeric_matrix &Hm,numeric_matrix &phiOne, numeric_matrix &phiTwo) // Builds phiOne and phiTwo
+	{
+		numeric_matrix exponential(arnoldiSize);
+		numeric_matrix inverse(arnoldiSize);
+		phiOne = Hm;
+		phiOne *= this->getTimeStepSize();
+
+		exponential = padeApproximation(phiOne);
+		exponential.add(makeIdentityMatrix(),(-1));
+
+		phiOne.gauss_jordan();
+
+		inverse = phiOne;
+
+		exponential.mmult(phiOne,inverse);
+
+		phiTwo = phiOne;
+		phiTwo.add(makeIdentityMatrix(),(-1));
+
+		exponential = phiTwo;
+		exponential.mmult(phiTwo,inverse);
+
+	}
+
 
 template double ExponentialTimeIntegrator<distributed_sparse_matrix,
 		distributed_vector>::step(distributed_vector& f,
@@ -180,4 +333,12 @@ template double ExponentialTimeIntegrator<distributed_sparse_block_matrix,
 		distributed_block_vector>::step(distributed_block_vector& f,
 		const distributed_sparse_block_matrix& systemMatrix,
 		const distributed_block_vector& systemVector, double t, double dt);
+
+template class ExponentialTimeIntegrator<distributed_sparse_block_matrix, distributed_block_vector>;
+template class ExponentialTimeIntegrator<distributed_sparse_matrix, distributed_vector>;
+
+
+
+
+
 } /* namespace natrium */
