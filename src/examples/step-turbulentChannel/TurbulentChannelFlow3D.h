e703559b574f15335e851438f31c789ba01743d7
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-01-06 17:00:03 +0100

    Laminar 3D Channel
    Former-commit-id: 10065fb09c3e76285424b0c99e374ec5829094d2 [formerly fbfef776e2f0fb4a0b2702bccd635d35b5c5e640]
    Former-commit-id: 6764946c20455cd2b426d98e75b33e820d0a4363

---
+++ src/examples/step-turbulentChannel/TurbulentChannelFlow3D.h
@@ -0,0 +1,73 @@
+/**
+ * @file TurbulentChannelFlow3D.h
+ * @short Channel flow in 3D
+ * @date 06.01.2016
+ * @author Andreas Kraemer, Bonn-Rhein-Sieg University of Applied Sciences, Sankt Augustin
+ */
+
+#ifndef TURBULENTCHANNELFLOW3D_H_
+#define TURBULENTCHANNELFLOW3D_H_
+
+#include "deal.II/grid/tria.h"
+#include "deal.II/base/function.h"
+
+#include "natrium/problemdescription/ProblemDescription.h"
+#include "natrium/utilities/BasicNames.h"
+
+
+
+namespace natrium {
+
+/** @short Description of a turbulent Channel Flow
+ *  The domain is [0,5]x[0,1].
+ */
+class TurbulentChannelFlow3D: public ProblemDescription<3> {
+public:
+
+	/**
+	 * @short class to describe the x-component of the analytic solution
+	 * @note other are default (v0=w0=0, rho0=1)
+	 */
+	class InitialVelocity: public dealii::Function<3> {
+	private:
+		TurbulentChannelFlow3D* m_flow;
+	public:
+		InitialVelocity(TurbulentChannelFlow3D* flow) :
+				m_flow(flow) {
+		}
+		virtual double value(const dealii::Point<3>& x, const unsigned int component=0) const;
+	};
+
+	/// constructor
+	TurbulentChannelFlow3D(double viscosity, size_t refinementLevel, double u_bulk = 1.0,
+			double height = 1.0, double length = 10.0, double width = 3.0, bool is_periodic = true);
+
+	/// destructor
+	virtual ~TurbulentChannelFlow3D();
+
+
+	virtual double getCharacteristicVelocity() const {
+		return m_uBulk;
+	}
+
+private:
+
+	double m_uBulk;
+
+	/**
+	 * @short create triangulation for couette flow
+	 * @return shared pointer to a triangulation instance
+	 */
+	boost::shared_ptr<Mesh<3> > makeGrid(double height, double length, double width);
+
+	/**
+	 * @short create boundaries for couette flow
+	 * @return shared pointer to a vector of boundaries
+	 * @note All boundary types are inherited of BoundaryDescription; e.g. PeriodicBoundary
+	 */
+	boost::shared_ptr<BoundaryCollection<3> > makeBoundaries(bool is_periodic);
+
+};
+
+} /* namespace natrium */
+#endif /* PERIODICFLOW2D_H_ */
---
+++ src/examples/step-turbulentChannel/step-turbulent-channel.cpp
@@ -0,0 +1,95 @@
+/**
+ * @file step-2.cpp
+ * @short Second tutorial:  Poiseuille Flow in 2D
+ * @date 24.10.2013
+ * @author Andreas Kraemer, Bonn-Rhein-Sieg University of Applied Sciences, Sankt Augustin
+ */
+
+#include <fstream>
+
+#include "TurbulentChannelFlow3D.h"
+
+#include "deal.II/numerics/data_out.h"
+
+#include "natrium/solver/CFDSolver.h"
+#include "natrium/solver/SolverConfiguration.h"
+
+#include "natrium/stencils/D3Q19.h"
+
+#include "natrium/problemdescription/ProblemDescription.h"
+
+#include "natrium/utilities/BasicNames.h"
+#include "natrium/utilities/CFDSolverUtilities.h"
+
+
+using namespace natrium;
+
+// Main function
+int main(int argc, char** argv) {
+
+	MPIGuard::getInstance(argc, argv);
+
+	pout << "Starting NATriuM step-turbulent-channel..." << endl;
+
+	const double CFL = 0.4;
+	const double Re = 100;
+	const double u_bulk = 0.0001 / 1.5; //1.0;
+	const double height = 1.0;
+	const double length = 10.0;
+	const double width = 3.0;
+	const double orderOfFiniteElement = 2;
+	const double Ma = atof(argv[1]);
+	const double refinement_level = atoi(argv[2]);
+	bool is_periodic = true;
+
+	/// create CFD problem
+	double viscosity  = u_bulk * height / Re;
+	const double scaling = sqrt(3) * 1.5 * u_bulk / Ma;
+	boost::shared_ptr<ProblemDescription<3> > channel3D = boost::make_shared<
+			TurbulentChannelFlow3D>(viscosity, refinement_level, u_bulk, height,
+			length, width, is_periodic);
+	const double dt = CFDSolverUtilities::calculateTimestep<3>(
+			*channel3D->getMesh(), orderOfFiniteElement, D3Q19(scaling), CFL);
+	//viscosity = 0.5*dt*scaling*scaling/3.; //u_bulk * height / Re;
+	//poiseuille2D->setViscosity(viscosity);
+	//poiseuille2D->getExternalForce()->scale(viscosity);
+
+
+	/// setup configuration
+	std::stringstream dirName;
+	dirName << getenv("NATRIUM_HOME") << "/turbulent-channel3D";
+	boost::shared_ptr<SolverConfiguration> configuration = boost::make_shared<
+			SolverConfiguration>();
+	//configuration->setSwitchOutputOff(true);
+	configuration->setOutputDirectory(dirName.str());
+	configuration->setRestartAtLastCheckpoint(false);
+	configuration->setUserInteraction(false);
+	configuration->setOutputTableInterval(10);
+	configuration->setOutputCheckpointInterval(100000000);
+	configuration->setOutputSolutionInterval(100);
+	configuration->setCommandLineVerbosity(WELCOME);
+	configuration->setSedgOrderOfFiniteElement(orderOfFiniteElement);
+	configuration->setStencilScaling(scaling);
+	configuration->setCommandLineVerbosity(ALL);
+	configuration->setTimeStepSize(dt);
+	configuration->setStencil(Stencil_D3Q19);
+	//configuration->setTimeIntegrator(OTHER);
+	//configuration->setDealIntegrator(CRANK_NICOLSON);
+
+	//configuration->setInitializationScheme(ITERATIVE);
+	//configuration->setIterativeInitializationNumberOfIterations(100);
+	//configuration->setIterativeInitializationResidual(1e-15);
+
+	configuration->setConvergenceThreshold(1e-10);
+
+	// make solver object and run simulation
+	CFDSolver<3> solver(configuration, channel3D);
+	solver.run();
+
+	pout << "Max Velocity  " <<
+			solver.getMaxVelocityNorm() << "   (laminar: "<< 1.5*u_bulk << ")" <<  endl;
+
+	pout << "NATriuM step-turbulent-channel terminated." << endl;
+
+	return 0;
+}
