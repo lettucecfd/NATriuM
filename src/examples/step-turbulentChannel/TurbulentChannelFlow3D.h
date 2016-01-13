fead7c96bf5e79c9b293eb1c1a136026ddf566c9
Zawadzki <gk629@slc1.cm.cluster>
2016-01-13 19:39:53 +0100

    STG:	Functions used by synthetic turbulence generator declared 	new member variables declared 	new gets implemented 	constructor enhanced with U_in
    Former-commit-id: 7a68794874612c0c7875e33e61823b36eb1b4743 [formerly 2eae62b59b3594c8205a06b04d272c238bb5368f]
    Former-commit-id: bb6af004e8180d355481378d9bb062c680bac9f6

--- src/examples/step-turbulentChannel/TurbulentChannelFlow3D.h
+++ src/examples/step-turbulentChannel/TurbulentChannelFlow3D.h
@@ -36,10 +36,11 @@ public:
 				m_flow(flow) {
 		}
 		virtual double value(const dealii::Point<3>& x, const unsigned int component=0) const;
+
 	};
 
 	/// constructor
-	TurbulentChannelFlow3D(double viscosity, size_t refinementLevel, double u_bulk = 1.0,
+	TurbulentChannelFlow3D(double viscosity, size_t refinementLevel, double u_bulk = 1.0, double U_in = 1.0,
 			double height = 1.0, double length = 10.0, double width = 3.0, bool is_periodic = true);
 
 	/// destructor
@@ -49,10 +50,27 @@ public:
 	virtual double getCharacteristicVelocity() const {
 		return m_uBulk;
 	}
+	//TODO: do we really need this?!
+	double getRefinementLevel() const {
+		return m_refinementLevel;
+	}
+	double getInletVelocity() const {
+		return m_Uin;
+	}
+
+	// Functions used by synthetic turbulence generator
+	inline void mean(vector<vector<double> > &matrix, double &avg);
+	inline void angles(int nmodes, int &iseed, int &iy, vector<int> &iv, vector<double> &fi,
+		vector<double> &psi, vector<double> &alfa, vector<double> &teta);
+	inline void randf_1(int nmd, int alow, double ahigh, int idum, int &iy, vector<int> &iv,
+		vector<double> &out, int &iseed);
+	inline void randf_2(int idum, int &iy, vector<int> &iv, double &ran1, int &iseed);
 
 private:
 
+	double m_refinementLevel;
 	double m_uBulk;
+	double m_Uin;
 
 	/**
 	 * @short create triangulation for couette flow
