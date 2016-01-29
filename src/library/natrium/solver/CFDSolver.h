769e4e36f752df0ea8b475af58c1ae44b0b35719
Zawadzki <gk629@slc1.cm.cluster>
2016-01-29 19:39:29 +0100

    STG: Functions scaleVelocity & addToVelocity implemented
    Former-commit-id: 0900be95e19c001b6143df7e657e8da3efbf813e [formerly fbef9057d3cbf569548cb14b18b1989b4028bd0f]
    Former-commit-id: e5c5ed0e1e4677c467e388bc2447292efadc0372

--- src/library/natrium/solver/CFDSolver.h
+++ src/library/natrium/solver/CFDSolver.h
@@ -126,6 +126,9 @@ private:
 	// vector for nonlinear boundary conditions
 	distributed_block_vector m_boundaryVector;
 
+	// support points
+	map<dealii::types::global_dof_index, dealii::Point<dim> > m_supportPoints;
+
 protected:
 
 	/// save the distribution functions to files for checkpointing
@@ -306,6 +309,18 @@ public:
 
 	void printRuntimeSummary() const {
 		pout << Timing::getOutStream().str() << endl;
+	}
+
+	void addToVelocity(boost::shared_ptr<dealii::Function<dim> > function);
+
+	void scaleVelocity(double scaling_factor);
+
+	const map<dealii::types::global_dof_index, dealii::Point<dim> >& getSupportPoints() const {
+		return m_supportPoints;
+	}
+
+	void setVelocity(const vector<distributed_vector>& velocity) {
+		m_velocity = velocity;
 	}
 }
 ;
