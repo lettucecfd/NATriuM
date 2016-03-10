c54f9a76cad4f24378deb5216da6b5de50b7708a
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-03-10 08:29:49 +0100

    TGV 3D improved
    Former-commit-id: a73edb771f41a9ec2ae78a70a4721bd9b365ace3 [formerly f7657fa3198b2468eff92f816a8912bc4844bf62]
    Former-commit-id: 0f3374fcef149e7eb7713a796044e6f78803f607

--- src/library/natrium/benchmarks/TaylorGreenVortex3D.h
+++ src/library/natrium/benchmarks/TaylorGreenVortex3D.h
@@ -16,7 +16,7 @@
 
 #include "deal.II/grid/tria.h"
 
-#include "../problemdescription/Benchmark.h"
+#include "../problemdescription/ProblemDescription.h"
 #include "../utilities/BasicNames.h"
 
 
@@ -27,31 +27,46 @@ namespace natrium {
  *  The domain is [0,1]^2. The domain consists of
  *  8 x 8 = 64 Elements (contrast to Min and Lee, who have 6 x 6).
  */
-class TaylorGreenVortex3D: public Benchmark<3> {
+class TaylorGreenVortex3D: public ProblemDescription<3> {
 public:
 
 	/**
 	 * @short class to describe the x-component of the analytic solution
 	 * @note other are default (v0=w0=0, rho0=1)
 	 */
-	class AnalyticVelocity: public dealii::Function<3> {
+	class InitialVelocity: public dealii::Function<3> {
 	private:
 		TaylorGreenVortex3D* m_flow;
 	public:
-		AnalyticVelocity(TaylorGreenVortex3D* flow) :
+		InitialVelocity(TaylorGreenVortex3D* flow) :
+				m_flow(flow) {
+		}
+		virtual double value(const dealii::Point<3>& x, const unsigned int component=0) const;
+	};
+	class InitialDensity: public dealii::Function<3> {
+	private:
+		TaylorGreenVortex3D* m_flow;
+	public:
+		InitialDensity(TaylorGreenVortex3D* flow) :
 				m_flow(flow) {
 		}
 		virtual double value(const dealii::Point<3>& x, const unsigned int component=0) const;
 	};
 
+
   /// constructor
   TaylorGreenVortex3D(double viscosity,
-      size_t refinementLevel);
+      size_t refinementLevel,  double cs = 0.57735026919, bool init_rho_analytically = false);
 
   /// destructor
   virtual ~TaylorGreenVortex3D();
 
 private:
+	/// speed of sound
+	double m_cs;
+
+	/// initialization with analytic pressure
+	bool m_analyticInit;
 
   /**
    * @short create triangulation for couette flow
