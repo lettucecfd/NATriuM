a43ef6e8440915c7891ee82127125613c3d9c17b
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-05-04 10:20:43 +0200

    SL Streaming: Neighborhood construction improved - now you can tell the function how many shells of neighbors you want.
    Began with following the Lagrangian path
    
    Former-commit-id: 125907f05f870c53eebf413dfd1d42974fecf413 [formerly 558b528d296abcd6d9f8758e412570476a9f53f2]
    Former-commit-id: cde97f65623b91afbb61569c7ccd40b014fee1d4

--- src/library/natrium/advection/SemiLagrangian.cpp
+++ src/library/natrium/advection/SemiLagrangian.cpp
@@ -25,6 +25,7 @@
 #include "../stencils/Stencil.h"
 
 #include "../utilities/DealiiExtensions.h"
+#include "../utilities/CFDSolverUtilities.h"
 
 using namespace dealii;
 
@@ -34,10 +35,10 @@ template<size_t dim>
 SemiLagrangian<dim>::SemiLagrangian(boost::shared_ptr<Mesh<dim> > triangulation,
 		boost::shared_ptr<BoundaryCollection<dim> > boundaries,
 		size_t orderOfFiniteElement, boost::shared_ptr<Stencil> Stencil,
-		bool useCentralFlux) :
+		double delta_t) :
 		m_mesh(triangulation), m_boundaries(boundaries), m_mapping(
 				orderOfFiniteElement), m_stencil(Stencil), m_orderOfFiniteElement(
-				orderOfFiniteElement), m_useCentralFlux(useCentralFlux) {
+				orderOfFiniteElement), m_deltaT(delta_t) {
 	// assertions
 	assert(orderOfFiniteElement >= 1);
 	assert(Stencil->getD() == dim);
@@ -81,17 +82,17 @@ void SemiLagrangian<dim>::reassemble() {
 // TODO: if Mesh changed: reinit dof-handler and sparsity pattern in some way
 
 	// make sure that sparsity structure is not empty
-	assert (m_systemMatrix.n() != 0);
-	assert (m_systemMatrix.m() != 0);
+	assert(m_systemMatrix.n() != 0);
+	assert(m_systemMatrix.m() != 0);
 
 /////////////////////////////////
 // Initialize Finite Element ////
 /////////////////////////////////
 // Define update flags (which values have to be known at each cell, face, neighbor face)
 	const dealii::UpdateFlags cellUpdateFlags = update_values
-	| update_quadrature_points | update_JxW_values;
-	const dealii::UpdateFlags faceUpdateFlags = update_quadrature_points | update_JxW_values
-			| update_normal_vectors;
+			| update_quadrature_points | update_JxW_values;
+	const dealii::UpdateFlags faceUpdateFlags = update_quadrature_points
+			| update_JxW_values | update_normal_vectors;
 // Finite Element
 	dealii::FEValues<dim> feCellValues(m_mapping, *m_fe, *m_quadrature,
 			cellUpdateFlags);
@@ -122,30 +123,7 @@ void SemiLagrangian<dim>::reassemble() {
 			m_doFHandler->begin_active(), endc = m_doFHandler->end();
 	for (; cell != endc; ++cell) {
 		if (cell->is_locally_owned()) {
-			// calculate the fe values for the cell
-			feCellValues.reinit(cell);
 
-			// get global degrees of freedom
-			cell->get_dof_indices(localDoFIndices);
-
-			// assemble local cell matrices
-			assembleLocalMassMatrix(feCellValues, dofs_per_cell,
-					localMassMatrix);
-			assembleLocalDerivativeMatrices(feCellValues, dofs_per_cell,
-					localDerivativeMatrices);
-
-			// invert local mass matrix
-			for (size_t i = 0; i < dofs_per_cell; i++) {
-				inverseLocalMassMatrix.at(i) = 1. / localMassMatrix.at(i);
-			}
-
-			// assemble faces and put together
-			for (size_t alpha = 1; alpha < m_stencil->getQ(); alpha++) {
-				// calculate local diagonal block (cell) matrix -D
-				calculateAndDistributeLocalStiffnessMatrix(alpha,
-						localDerivativeMatrices, localSystemMatrix,
-						inverseLocalMassMatrix, localDoFIndices, dofs_per_cell);
-			}
 		}
 	}
 	m_systemVector.compress(dealii::VectorOperation::add);
@@ -283,218 +261,6 @@ void SemiLagrangian<dim>::updateSparsityPattern() {
 }
 /* updateSparsityPattern */
 
-template<size_t dim>
-void SemiLagrangian<dim>::assembleLocalMassMatrix(
-		const dealii::FEValues<dim>& feValues, size_t dofs_per_cell,
-		vector<double> &massMatrix) {
-// initialize with zeros
-	std::fill(massMatrix.begin(), massMatrix.end(), 0.0);
-
-// fill diagonal "matrix"
-	for (size_t i = 0; i < dofs_per_cell; i++) {
-		size_t q_point = m_celldof_to_q_index.at(i);
-		massMatrix.at(i) += feValues.shape_value(i, q_point)
-				* feValues.shape_value(i, q_point) * feValues.JxW(q_point);
-	}
-
-} /*assembleLocalMassMatrix*/
-
-template<size_t dim>
-void SemiLagrangian<dim>::assembleLocalDerivativeMatrices(
-		const dealii::FEValues<dim>& feValues, size_t dofs_per_cell,
-		vector<dealii::FullMatrix<double> >&derivativeMatrix) const {
-	for (size_t i = 0; i < dim; i++) {
-		derivativeMatrix.at(i) = 0;
-	}
-	for (size_t i = 0; i < dofs_per_cell; i++) {
-		for (size_t j = 0; j < dofs_per_cell; j++) {
-			// the shape value is zero for all q, except i<->q
-			size_t q_point = m_celldof_to_q_index.at(i);
-			Tensor<1, dim> integrandAtQ;
-			integrandAtQ = feValues.shape_grad(j, q_point);
-			integrandAtQ *= (feValues.shape_value(i, q_point)
-					* feValues.JxW(q_point));
-			for (size_t k = 0; k < dim; k++) {
-				derivativeMatrix.at(k)(i, j) += integrandAtQ[k];
-			}
-
-		}
-	}
-
-} /* assembleLocalDerivativeMatrix */
-
-template<size_t dim>
-void SemiLagrangian<dim>::assembleAndDistributeLocalFaceMatrices(size_t alpha,
-		typename dealii::DoFHandler<dim>::active_cell_iterator& cell,
-		dealii::FEFaceValues<dim>& feFaceValues,
-		dealii::FESubfaceValues<dim>& feSubfaceValues,
-		dealii::FEFaceValues<dim>& feNeighborFaceValues,
-		const vector<double>& inverseLocalMassMatrix) {
-
-// loop over all faces
-	for (size_t j = 0; j < dealii::GeometryInfo<dim>::faces_per_cell; j++) {
-		//Faces at boundary
-		if (cell->face(j)->at_boundary()) {
-			size_t boundaryIndicator = cell->face(j)->boundary_id();
-			if (m_boundaries->isPeriodic(boundaryIndicator)) {
-				// Apply periodic boundaries
-				const boost::shared_ptr<PeriodicBoundary<dim> >& periodicBoundary =
-						m_boundaries->getPeriodicBoundary(boundaryIndicator);
-				assert(periodicBoundary->isFaceInBoundary(cell, j));
-				typename dealii::DoFHandler<dim>::cell_iterator neighborCell;
-				size_t opposite_face =
-						periodicBoundary->getOppositeCellAtPeriodicBoundary(
-								cell, neighborCell);
-				assembleAndDistributeInternalFace(alpha, cell, j, neighborCell,
-						opposite_face, feFaceValues, feSubfaceValues,
-						feNeighborFaceValues, inverseLocalMassMatrix);
-			} else /* if is not periodic */{
-				// Apply other boundaries
-				if ((m_boundaries->getBoundary(boundaryIndicator)->isLinear())) {
-					const boost::shared_ptr<LinearBoundary<dim> >& LinearBoundary =
-							m_boundaries->getLinearBoundary(boundaryIndicator);
-					LinearBoundary->assembleBoundary(alpha, cell, j,
-							feFaceValues, *m_stencil,
-							m_q_index_to_facedof.at(j), inverseLocalMassMatrix,
-							m_systemMatrix, m_systemVector, m_useCentralFlux);
-				}
-			} /* endif isPeriodic */
-
-		} else /* if is not at boundary */{
-// Internal faces
-			typename DoFHandler<dim>::cell_iterator neighbor = cell->neighbor(
-					j);
-			assembleAndDistributeInternalFace(alpha, cell, j, neighbor,
-					cell->neighbor_face_no(j), feFaceValues, feSubfaceValues,
-					feNeighborFaceValues, inverseLocalMassMatrix);
-		} /* endif is face at boundary*/
-
-	}
-
-} /* assembleLocalFaceMatrix */
-
-template<> void SemiLagrangian<2>::calculateAndDistributeLocalStiffnessMatrix(
-		size_t alpha,
-		const vector<dealii::FullMatrix<double> >& derivativeMatrices,
-		dealii::FullMatrix<double> &systemMatrix,
-		const vector<double>& inverseLocalMassMatrix,
-		const std::vector<dealii::types::global_dof_index>& globalDoFs,
-		size_t dofsPerCell) {
-// TODO efficient implementation (testing if e_ix, e_iy = 0, -1 or 1)
-// calculate -D = -(e_x * D_x  +  e_y * D_y)
-	systemMatrix = derivativeMatrices.at(0);
-	systemMatrix *= (-(m_stencil->getDirection(alpha)[0]));
-	systemMatrix.add(-(m_stencil->getDirection(alpha)[1]),
-			derivativeMatrices.at(1));
-// distribute to global system matrix
-	// avoid to call block() too often
-	distributed_sparse_matrix& block = m_systemMatrix.block(alpha - 1,
-			alpha - 1);
-	for (unsigned int j = 0; j < dofsPerCell; j++)
-		for (unsigned int k = 0; k < dofsPerCell; k++) {
-			block.add(globalDoFs[j], globalDoFs[k],
-					systemMatrix(j, k) * inverseLocalMassMatrix.at(j));
-		}
-}
-template<> void SemiLagrangian<3>::calculateAndDistributeLocalStiffnessMatrix(
-		size_t alpha,
-		const vector<dealii::FullMatrix<double> >& derivativeMatrices,
-		dealii::FullMatrix<double> &systemMatrix,
-		const vector<double>& inverseLocalMassMatrix,
-		const std::vector<dealii::types::global_dof_index>& globalDoFs,
-		size_t dofsPerCell) {
-// TODO efficient implementation (testing if e_ix, e_iy = 0, -1 or 1)
-// calculate -D = -(e_x * D_x  +  e_y * D_y)
-	systemMatrix = derivativeMatrices.at(0);
-	systemMatrix *= (-m_stencil->getDirection(alpha)[0]);
-	systemMatrix.add(-m_stencil->getDirection(alpha)[1],
-			derivativeMatrices.at(1), -m_stencil->getDirection(alpha)[2],
-			derivativeMatrices.at(2));
-// distribute to global system matrix
-	distributed_sparse_matrix& block = m_systemMatrix.block(alpha - 1,
-			alpha - 1);
-	for (unsigned int j = 0; j < dofsPerCell; j++)
-		for (unsigned int k = 0; k < dofsPerCell; k++)
-			block.add(globalDoFs[j], globalDoFs[k],
-					systemMatrix(j, k) * inverseLocalMassMatrix.at(j));
-}
-
-template<size_t dim>
-void SemiLagrangian<dim>::assembleAndDistributeInternalFace(size_t alpha,
-		typename dealii::DoFHandler<dim>::active_cell_iterator& cell,
-		size_t faceNumber,
-		typename dealii::DoFHandler<dim>::cell_iterator& neighborCell,
-		size_t neighborFaceNumber, dealii::FEFaceValues<dim>& feFaceValues,
-		dealii::FESubfaceValues<dim>& feSubfaceValues,
-		dealii::FEFaceValues<dim>& feNeighborFaceValues,
-		const vector<double>& inverseLocalMassMatrix) {
-// get the required FE Values for the local cell
-	feFaceValues.reinit(cell, faceNumber);
-	const vector<double> &JxW = feFaceValues.get_JxW_values();
-	const vector<Tensor<1, dim> > &normals =
-			feFaceValues.get_all_normal_vectors();
-
-	if (4 == alpha) {
-
-	}
-// get the required dofs of the neighbor cell
-//	typename DoFHandler<dim>::face_iterator neighborFace = neighborCell->face(
-//			neighborFaceNumber);
-	feNeighborFaceValues.reinit(neighborCell, neighborFaceNumber);
-
-	vector<dealii::types::global_dof_index> localDoFIndices(
-			feFaceValues.get_fe().dofs_per_cell);
-	vector<dealii::types::global_dof_index> neighborDoFIndices(
-			feFaceValues.get_fe().dofs_per_cell);
-	cell->get_dof_indices(localDoFIndices);
-	neighborCell->get_dof_indices(neighborDoFIndices);
-
-	// avoid many calls to block()
-	distributed_sparse_matrix& block = m_systemMatrix.block(alpha - 1,
-			alpha - 1);
-
-// loop over all quadrature points at the face
-	for (size_t q = 0; q < feFaceValues.n_quadrature_points; q++) {
-		size_t thisDoF = m_q_index_to_facedof.at(faceNumber).at(q);
-		assert(feFaceValues.shape_value(thisDoF, q) > 0);
-		size_t neighborDoF = m_q_index_to_facedof.at(neighborFaceNumber).at(q);
-		assert(feNeighborFaceValues.shape_value(neighborDoF, q) > 0);
-
-		double cell_entry = 0.0;
-		double neighbor_entry = 0.0;
-
-		// calculate matrix entries
-		double prefactor = JxW.at(q);
-		double exn = 0.0;
-
-		// calculate scalar product
-		for (size_t i = 0; i < dim; i++) {		// TODO efficient multiplication
-			exn += normals.at(q)[i] * m_stencil->getDirection(alpha)(i);
-		}
-		prefactor *= exn;
-
-		if (m_useCentralFlux) {
-			cell_entry = 0.5 * prefactor;
-			neighbor_entry = 0.5 * prefactor;
-
-		} else if (exn < 0) { // otherwise: no contributions
-			cell_entry = prefactor;
-			neighbor_entry = -prefactor;
-		}
-
-		block.add(localDoFIndices[thisDoF], localDoFIndices[thisDoF],
-				cell_entry * inverseLocalMassMatrix.at(thisDoF));
-		block.add(localDoFIndices[thisDoF], neighborDoFIndices[neighborDoF],
-				neighbor_entry * inverseLocalMassMatrix.at(thisDoF));
-	}
-
-// get DoF indices
-// TODO cut out construction (allocation); Allocating two vectors in most inner loop is too expensive
-
-// WARNING: Hanging nodes are not implemented, yet
-// TODO Implement local refinement
-
-} /* assembleAndDistributeInternalFace */
 
 template<size_t dim>
 std::map<size_t, size_t> SemiLagrangian<dim>::map_celldofs_to_q_index() const {
@@ -593,6 +359,89 @@ vector<std::map<size_t, size_t> > SemiLagrangian<dim>::map_q_index_to_facedofs()
 template<size_t dim>
 void SemiLagrangian<dim>::stream() {
 }
+
+template<size_t dim>
+void SemiLagrangian<dim>::fillSparseObject(bool sparsity_pattern) {
+
+	if (not sparsity_pattern) {
+		// make sure that sparsity structure is not empty
+		assert(m_systemMatrix.n() != 0);
+		assert(m_systemMatrix.m() != 0);
+
+	}
+
+	/////////////////////////////////
+	// Initialize Finite Element ////
+	/////////////////////////////////
+	// Define update flags (which values have to be known at each cell, face, neighbor face)
+	const dealii::UpdateFlags cell_update_flags = update_values
+			| update_quadrature_points | update_JxW_values;
+	const dealii::UpdateFlags face_update_flags = update_quadrature_points
+			| update_normal_vectors;
+	// Finite Element
+	dealii::FEValues<dim> fe_cell_values(m_mapping, *m_fe, *m_quadrature,
+			cell_update_flags);
+	dealii::FEFaceValues<dim> fe_face_values(m_mapping, *m_fe, *m_faceQuadrature,
+			face_update_flags);
+
+	// Initialize
+	const size_t dofs_per_cell = m_fe->dofs_per_cell;
+	const std::vector<Point<dim> > & unit_support_points =
+			m_fe->get_unit_support_points();
+	std::vector<Tensor<1, dim> > minus_dtealpha;
+	for (size_t i = 0; i < m_stencil->getQ(); i++) {
+		minus_dtealpha.push_back(
+				vectorToTensor(m_stencil->getDirection(i)) * (-m_deltaT));
+	}
+	std::vector<dealii::types::global_dof_index> local_dof_indices(
+			dofs_per_cell);
+	std::vector<dealii::types::global_dof_index> neighbor_dof_indices(
+			dofs_per_cell);
+	size_t max_n_shells = (size_t) (m_stencil->getMaxParticleVelocityMagnitude()
+			* m_deltaT
+			/ CFDSolverUtilities::getMinimumVertexDistance<dim>(*m_mesh)) + 1;
+	if ((max_n_shells > 1) and (dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD)>1)){
+		LOG(WARNING) << "The global CFL number (wrt. cells) is > 1. That is not a problem, in principle. "
+				"However, depending on the mesh and its distribution, the simulation may crash, as the "
+				"semi-Lagrangian paths may lead into cells that are not even ghost cells. "
+				"When this happens, the algorithm is not longer defined and you will get an error message "
+				"before the simulation crashes.";
+	}
+
+	///////////////
+	// MAIN LOOP //
+	///////////////
+	typename DoFHandler<dim>::active_cell_iterator cell =
+			m_doFHandler->begin_active(), endc = m_doFHandler->end();
+	for (; cell != endc; ++cell) {
+		if (cell->is_locally_owned()) {
+			// calculate the fe values for the cell
+			fe_cell_values.reinit(cell);
+
+			// get global degrees of freedom
+			cell->get_dof_indices(local_dof_indices);
+
+			// for all points in cell
+			for (size_t i = 0; i < dofs_per_cell; i++) {
+				// get a point x
+				dealii::Point<dim> x_i = m_mapping.transform_unit_to_real_cell(
+						cell, unit_support_points.at(i));
+				// for all directions
+				for (size_t alpha = 1; alpha < m_stencil->getQ(); alpha++) {
+					// calculate x^(t-delta_t)
+					dealii::Point<dim> x_previous = x_i + minus_dtealpha.at(alpha);
+					dealii::Point<dim> null;
+
+				}
+			}
+		}
+
+	}
+	m_systemVector.compress(dealii::VectorOperation::add);
+	m_systemMatrix.compress(dealii::VectorOperation::add);
+
+//#endif
+} /* fillSparseObject */
 
 template class SemiLagrangian<2> ;
 template class SemiLagrangian<3> ;
--- src/test/advection/SemiLagrangian_test.cpp
+++ src/test/advection/SemiLagrangian_test.cpp
@@ -36,7 +36,7 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian_Construction_test) {
 	size_t refinementLevel = 2;
 	PeriodicTestDomain2D periodic(refinementLevel);
 	BOOST_CHECK_NO_THROW(
-			SemiLagrangian<2> streaming(periodic.getMesh(), periodic.getBoundaries(), fe_order, boost::make_shared<D2Q9>()));
+			SemiLagrangian<2> streaming(periodic.getMesh(), periodic.getBoundaries(), fe_order, boost::make_shared<D2Q9>(), 0.001));
 
 	pout << "done." << endl;
 } /* SemiLagrangian_Construction_test */
@@ -49,18 +49,14 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian2D_Neighborhood_test) {
 	PeriodicTestDomain2D periodic(refinementLevel);
 	periodic.refineAndTransform();
 	SemiLagrangian<2> sl(periodic.getMesh(), periodic.getBoundaries(), fe_order,
-			boost::make_shared<D2Q9>());
+			boost::make_shared<D2Q9>(), 0.001);
 	sl.setupDoFs();
 	typename dealii::DoFHandler<2>::active_cell_iterator cell =
 			sl.getDoFHandler()->begin_active();
 	SemiLagrangian<2>::Neighborhood neighbors;
 	sl.getNeighborhood(cell, neighbors);
 	dealii::Point<2> cell_center = cell->barycenter();
-	if (dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1) {
-		BOOST_CHECK_EQUAL(neighbors.size(), 13);
-	} else {
-		BOOST_CHECK_GE(neighbors.size(), 9);
-	}
+	BOOST_CHECK_EQUAL(neighbors.size(), size_t(9));
 	double dx = pow(0.5, 3);
 	size_t n_periodic_x = 0;
 	size_t n_periodic_y = 0;
@@ -76,29 +72,22 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian2D_Neighborhood_test) {
 			n_periodic_y++;
 		}
 		double dist = cell_center.distance(n_center);
-		// check dist \in {0, dx, sqrt(2)*dx, 2*dx}
-		BOOST_CHECK_LE(dist, 2 * dx + 1e-8);
+		// check dist \in {0, dx, sqrt(2)*dx}
+		BOOST_CHECK_LE(dist, sqrt(2) * dx + 1e-8);
 		if (dist < dx - 1e-8) {
 			BOOST_CHECK_SMALL(dist, 1e-8);
 		} else if (dist < dx + 1e-8) {
 			BOOST_CHECK_GT(dist, dx - 1e-8);
-		} else if (dist < dx * sqrt(2) + 1e-8) {
+		} else  { // (dist < dx * sqrt(2) + 1e-8)
 			BOOST_CHECK_GT(dist, dx * sqrt(2) - 1e-8);
-		} else {
-			BOOST_CHECK_GT(dist, dx * 2 - 1e-8);
 		}
 	}
 	// cell is corner!
-	if (dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1) {
-		BOOST_CHECK_EQUAL(n_periodic_x, 4);
-		BOOST_CHECK_EQUAL(n_periodic_y, 4);
-	} else {
-		BOOST_CHECK_GE(n_periodic_x, 3);
-		BOOST_CHECK_GE(n_periodic_y, 3);
-	}
+	BOOST_CHECK_EQUAL(n_periodic_x, 3);
+	BOOST_CHECK_EQUAL(n_periodic_y, 3);
 
 	pout << "done." << endl;
-} /* SemiLagrangian2D_Neighborhod_test */
+} /* SemiLagrangian2D_Neighborhood_test */
 
 BOOST_AUTO_TEST_CASE(SemiLagrangian3D_Neighborhood_test) {
 	pout << "SemiLagrangian3D_Neighborhood_test..." << endl;
@@ -108,18 +97,14 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian3D_Neighborhood_test) {
 	PeriodicTestDomain3D periodic(refinementLevel);
 	periodic.refineAndTransform();
 	SemiLagrangian<3> sl(periodic.getMesh(), periodic.getBoundaries(), fe_order,
-			boost::make_shared<D3Q19>());
+			boost::make_shared<D3Q19>(), 0.001 );
 	sl.setupDoFs();
 	typename dealii::DoFHandler<3>::active_cell_iterator cell =
 			sl.getDoFHandler()->begin_active();
 	SemiLagrangian<3>::Neighborhood neighbors;
 	sl.getNeighborhood(cell, neighbors);
 	dealii::Point<3> cell_center = cell->barycenter();
-	if (dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1) {
-		BOOST_CHECK_EQUAL(neighbors.size(), 63);
-	} else {
-		BOOST_CHECK_GE(neighbors.size(), 27);
-	}
+	BOOST_CHECK_EQUAL(neighbors.size(), 27);
 	double dx = pow(0.5, 3);
 	size_t n_periodic_x = 0;
 	size_t n_periodic_y = 0;
@@ -140,46 +125,35 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian3D_Neighborhood_test) {
 			n_periodic_z++;
 		}
 		double dist = cell_center.distance(n_center);
-		// check dist \in {0, dx, sqrt(2)*dx sqrt(3)*dx, 2*dx}
-		BOOST_CHECK_LE(dist, 3 * dx + 1e-8);
+		// check dist \in {0, dx, sqrt(2)*dx sqrt(3)*dx}
+		BOOST_CHECK_LE(dist, sqrt(3) * dx + 1e-8);
 		if (dist < dx - 1e-8) {
 			BOOST_CHECK_SMALL(dist, 1e-8);
 		} else if (dist < dx + 1e-8) {
 			BOOST_CHECK_GT(dist, dx - 1e-8);
 		} else if (dist < dx * sqrt(2) + 1e-8) {
 			BOOST_CHECK_GT(dist, dx * sqrt(2) - 1e-8);
-		} else if (dist < dx * sqrt(3) + 1e-8) {
+		} else { // (dist < dx * sqrt(3) + 1e-8)
 			BOOST_CHECK_GT(dist, dx * sqrt(3) - 1e-8);
-		} else if (dist < dx * 2 + 1e-8) {
-			BOOST_CHECK_GT(dist, dx * 2 - 1e-8);
-		} else if (dist < dx * sqrt(5) + 1e-8) {
-			BOOST_CHECK_GT(dist, dx * sqrt(5) - 1e-8);
-		} else {
-			BOOST_CHECK_GT(dist, dx * 3 - 1e-8);
 		}
 	}
 	// cell is corner!
-	if (dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1) {
-		BOOST_CHECK_EQUAL(n_periodic_x, 19);
-		BOOST_CHECK_EQUAL(n_periodic_y, 19);
-		BOOST_CHECK_EQUAL(n_periodic_z, 19);
-	} else {
-		BOOST_CHECK_GE(n_periodic_x, 13);
-		BOOST_CHECK_GE(n_periodic_y, 13);
-	}
+	BOOST_CHECK_EQUAL(n_periodic_x, 9);
+	BOOST_CHECK_EQUAL(n_periodic_y, 9);
+	BOOST_CHECK_EQUAL(n_periodic_z, 9);
 
 	pout << "done." << endl;
-} /* SemiLagrangian3D_Neighborhod_test */
+} /* SemiLagrangian3D_Neighborhood_test */
 
-BOOST_AUTO_TEST_CASE(SemiLagrangian2D_FindPointInNeighborhod_test) {
-	pout << "SemiLagrangian2D_FindPointInNeighborhod_test..." << endl;
+BOOST_AUTO_TEST_CASE(SemiLagrangian2D_FindPointInNeighborhood_test) {
+	pout << "SemiLagrangian2D_FindPointInNeighborhood_test..." << endl;
 
 	size_t fe_order = 1;
 	size_t refinementLevel = 3;
 	PeriodicTestDomain2D periodic(refinementLevel);
 	periodic.refineAndTransform();
 	SemiLagrangian<2> sl(periodic.getMesh(), periodic.getBoundaries(), fe_order,
-			boost::make_shared<D2Q9>());
+			boost::make_shared<D2Q9>(), 0.001);
 	sl.setupDoFs();
 	typename dealii::DoFHandler<2>::active_cell_iterator cell =
 			sl.getDoFHandler()->begin_active();
@@ -194,17 +168,17 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian2D_FindPointInNeighborhod_test) {
 	BOOST_CHECK(found_in != sl.getDoFHandler()->end());
 
 	pout << "done." << endl;
-} /* SemiLagrangian2D_FindPointInNeighborhod_test */
+} /* SemiLagrangian2D_FindPointInNeighborhood_test */
 
-BOOST_AUTO_TEST_CASE(SemiLagrangian3D_FindPointInNeighborhod_test) {
-	pout << "SemiLagrangian3D_FindPointInNeighborhod_test..." << endl;
+BOOST_AUTO_TEST_CASE(SemiLagrangian3D_FindPointInNeighborhood_test) {
+	pout << "SemiLagrangian3D_FindPointInNeighborhood_test..." << endl;
 
 	size_t fe_order = 1;
 	size_t refinementLevel = 3;
 	PeriodicTestDomain3D periodic(refinementLevel);
 	periodic.refineAndTransform();
 	SemiLagrangian<3> sl(periodic.getMesh(), periodic.getBoundaries(), fe_order,
-			boost::make_shared<D3Q19>());
+			boost::make_shared<D3Q19>(), 0.001);
 	sl.setupDoFs();
 	typename dealii::DoFHandler<3>::active_cell_iterator cell =
 			sl.getDoFHandler()->begin_active();
@@ -219,7 +193,7 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian3D_FindPointInNeighborhod_test) {
 	BOOST_CHECK(found_in != sl.getDoFHandler()->end());
 
 	pout << "done." << endl;
-} /* SemiLagrangian3D_FindPointInNeighborhod_test */
+} /* SemiLagrangian3D_FindPointInNeighborhood_test */
 
 BOOST_AUTO_TEST_SUITE_END()
 
