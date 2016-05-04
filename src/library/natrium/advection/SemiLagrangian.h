a77a74bf761b19c77f0a39992ccf49d027fcc583
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-05-04 10:20:43 +0200

    SL Streaming: Neighborhood construction improved - now you can tell the function how many shells of neighbors you want.
    Began with following the Lagrangian path
    
    Former-commit-id: da0c5992617dd64c0560021bc44c46bb57f0e67e [formerly a203ad9212bd8acc90e23b8f71965f0eb508570d]
    Former-commit-id: b271cca6470013324d626bde3f123fe8c5d4d235

--- src/library/natrium/advection/SemiLagrangian.h
+++ src/library/natrium/advection/SemiLagrangian.h
@@ -9,6 +9,7 @@
 #define SEMILAGRANGIAN_H_
 
 #include <map>
+#include <array>
 
 #include "deal.II/grid/tria.h"
 #include "deal.II/fe/fe_dgq.h"
@@ -101,8 +102,8 @@ private:
 	/// order of the finite element functions
 	size_t m_orderOfFiniteElement;
 
-	/// central flux or Lax-Friedrichs flux (default)
-	const bool m_useCentralFlux;
+	// time step size
+	double m_deltaT;
 
 	// locally owned degrees of freedom (for MPI parallelization)
 	dealii::IndexSet m_locallyOwnedDofs;
@@ -111,70 +112,14 @@ private:
 	dealii::IndexSet m_locallyRelevantDofs;
 
 	/**
+	 * @short update the sparsity pattern of the system matrix // the sparse matrix
+	 */
+	void fillSparseObject(bool sparsity_pattern = false);
+
+	/**
 	 * @short update the sparsity pattern of the system matrix
 	 */
 	void updateSparsityPattern();
-
-	/**
-	 * @short assemble local mass matrix M
-	 * @param[out] massMatrix The mass matrix <phi_i, phi_j>
-	 *             For SEDG methods the mass Matrix is diagonal.
-	 *
-	 */
-	// TODO SEDG implemenation with fully diagonal mass matrix
-	void assembleLocalMassMatrix(const dealii::FEValues<dim>& feValues,
-			size_t dofs_per_cell, vector<double> &massMatrix);
-
-	/**
-	 * @short assemble the \f$\alpha\f$-th local derivative matrix
-	 * @param[in] coordinate < dim; 0 for Dx, 1 for Dy, 2 for Dz (3D)
-	 * @param[out] derivativeMatrix The i-th derivative matrix <D_i phi_j, phi_k>
-	 */
-	void assembleLocalDerivativeMatrices(const dealii::FEValues<dim>& feValues,
-			size_t dofs_per_cell,
-			vector<dealii::FullMatrix<double> > &derivativeMatrix) const;
-
-	/**
-	 * @short assemble local face matrix;
-	 * @param[in] alpha < Q; this matrix is dependent on the flow direction
-	 * @param[out] faceMatrix The integral over all faces, incorporating boundary conditions
-	 */
-	void assembleAndDistributeLocalFaceMatrices(size_t alpha,
-			typename dealii::DoFHandler<dim>::active_cell_iterator& cell,
-			dealii::FEFaceValues<dim>& feFaceValues,
-			dealii::FESubfaceValues<dim>& feSubfaceValues,
-			dealii::FEFaceValues<dim>& feNeighborFaceValues,
-			const vector<double>& inverseLocalMassMatrix);
-
-	/**
-	 * @short calculate system diagonal block matrix  (Dx*eix + Dy*eiy)
-	 */
-	void calculateAndDistributeLocalStiffnessMatrix(size_t alpha,
-			const vector<dealii::FullMatrix<double> > &derivativeMatrices,
-			dealii::FullMatrix<double> &systemMatrix,
-			const vector<double>& inverseLocalMassMatrix,
-			const std::vector<dealii::types::global_dof_index>& globalDoFs,
-			size_t dofsPerCell);
-
-	/**
-	 * @short assemble and distribute internal face
-	 * @param[in] alpha < Q;  the index of the particle flow direction
-	 * @param cell the cell to which the face belongs
-	 * @param faceNumber the local number of the face (0,1,2 or 3)
-	 * @param neighborCell the cell on the other side of the face
-	 * @param neighborFaceNumber the local number of the face (0,1,2 or 3), as seen from the neigbor cell
-	 * @param feFaceValues is passed in order to avoid allocating memory for each call of the function
-	 * @param feSubfaceValues is passed in order to avoid allocating memory for each call of the function
-	 * @param feNeighborFaceValues is passed in order to avoid allocating memory for each call of the function
-	 */
-	void assembleAndDistributeInternalFace(size_t alpha,
-			typename dealii::DoFHandler<dim>::active_cell_iterator& cell,
-			size_t faceNumber,
-			typename dealii::DoFHandler<dim>::cell_iterator& neighborCell,
-			size_t neighborFaceNumber, dealii::FEFaceValues<dim>& feFaceValues,
-			dealii::FESubfaceValues<dim>& feSubfaceValues,
-			dealii::FEFaceValues<dim>& feNeighborFaceValues,
-			const vector<double>& inverseLocalMassMatrix);
 
 	/**
 	 * @short map degrees of freedom to quadrature node indices on a cell
@@ -236,7 +181,7 @@ public:
 	SemiLagrangian(boost::shared_ptr<Mesh<dim> > triangulation,
 			boost::shared_ptr<BoundaryCollection<dim> > boundaries,
 			size_t orderOfFiniteElement, boost::shared_ptr<Stencil> stencil,
-			bool useCentralFlux = false);
+			double delta_t);
 
 	/// destructor
 	virtual ~SemiLagrangian() {
@@ -299,24 +244,23 @@ public:
 	 */
 	void getNeighborhood(
 			typename dealii::DoFHandler<dim>::active_cell_iterator& cell,
-			Neighborhood& neighborhood) {
+			Neighborhood& neighborhood, size_t n_shells = 1) {
+		const size_t faces_per_cell = 2 * dim;
 		neighborhood.clear();
+		vector < std::array<size_t, faces_per_cell> > visited_faces;
+
 		// add self
 		neighborhood.push_back(cell);
+		visited_faces.push_back(std::array<size_t, faces_per_cell>());
 		// as the neighborhood gets extended every time a new neighbor is added
 		// this loop runs over all cells (therefor we have to have a break)
-		size_t visited = 0;
-		size_t n_first_dim_shells = 5;
-		if (dim == 3){
-			n_first_dim_shells = 25;
-		}
 		for (size_t c = 0; c < neighborhood.size(); c++) {
-			if (visited >= n_first_dim_shells){
-				break;
-			}
-			visited ++;
-			for (size_t i = 0; i < dealii::GeometryInfo<dim>::faces_per_cell;
-					i++) {
+			for (size_t i = 0; i < faces_per_cell; i++) {
+				// TODO check the shells over cell orientation rather than face id
+				// The face-id test is risky for arbitrary meshes
+				if (visited_faces.at(c).at(i) >= n_shells) {
+					continue;
+				}
 				typename dealii::DoFHandler<dim>::cell_iterator n = getNeighbor(
 						neighborhood.at(c), i);
 				if (n == cell->get_dof_handler().end()) {
@@ -326,6 +270,9 @@ public:
 				} else if (isCellInNeighborhood(*n, neighborhood)) {
 					continue;
 				} else if (n->active() or n->is_ghost()) {
+					std::array < size_t, faces_per_cell > v(visited_faces.at(c));
+					v[i]++;
+					visited_faces.push_back(v);
 					neighborhood.push_back(
 							typename dealii::DoFHandler<dim>::active_cell_iterator(
 									n));
@@ -337,6 +284,9 @@ public:
 								neighborhood)) {
 							continue;
 						}
+						std::array < size_t, faces_per_cell > v(visited_faces.at(c));
+						v[i]++;
+						visited_faces.push_back(v);
 						neighborhood.push_back(
 								typename dealii::DoFHandler<dim>::active_cell_iterator(
 										n->child(j)));
@@ -353,13 +303,14 @@ public:
 	 * @param cell The start cell of the recursive search
 	 * @return A cell that contains the point p. If the point was not found, the cell pointer will point to DoFHandler.end()
 	 */
-	typename dealii::DoFHandler<dim>::active_cell_iterator recursivelySearchInNeighborhood(const dealii::Point<dim>& p,
+	typename dealii::DoFHandler<dim>::active_cell_iterator recursivelySearchInNeighborhood(
+			const dealii::Point<dim>& p,
 			typename dealii::DoFHandler<dim>::active_cell_iterator& cell) {
 		Neighborhood neighborhood;
 		// add self
 		neighborhood.push_back(cell);
 		for (size_t c = 0; c < neighborhood.size(); c++) {
-			if (neighborhood.at(c)->point_inside(p)){
+			if (neighborhood.at(c)->point_inside(p)) {
 				return neighborhood.at(c);
 			}
 			for (size_t i = 0; i < dealii::GeometryInfo<dim>::faces_per_cell;
@@ -394,7 +345,6 @@ public:
 		}
 		return cell->get_dof_handler().end();
 	} /* recursivelySearchInNeighborhood */
-
 
 	/// function to (re-)assemble linear system
 	virtual void reassemble();
@@ -481,6 +431,16 @@ public:
 
 	virtual const boost::shared_ptr<Stencil>& getStencil() const {
 		return m_stencil;
+	}
+
+	dealii::Tensor<1, dim> vectorToTensor(const numeric_vector& v) const {
+		assert(v.size() == dim);
+		dealii::Tensor<1,dim> t;
+		t[0] = v(0);
+		t[1] = v(1);
+		if (dim == 3)
+			t[2] = v(2);
+		return t;
 	}
 }
 ;
