da74e70b9f99e20fc92748bba967c6bebe36ffef
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-05-04 14:01:48 +0200

    SemiLagrangian: further progress - following the Lagrangian paths into the cells of their origin. Bugfix in parallel UnitTest of improved Neighborhood - now, the neighborhood works as one would expect. Former-commit-id: dcac27c496e36efa480d666638789437b263b209 [formerly 09b8f5d83b60c7126d61ddc9ade8f2dd07fa6aa9] Former-commit-id: 5d76cb694d94155268f07a58bed4221b9701cbc9

--- src/test/advection/SemiLagrangian_test.cpp
+++ src/test/advection/SemiLagrangian_test.cpp
@@ -53,38 +53,47 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian2D_Neighborhood_test) {
 	sl.setupDoFs();
 	typename dealii::DoFHandler<2>::active_cell_iterator cell =
 			sl.getDoFHandler()->begin_active();
+	// make sure cell is not in the ghost layer (begin_active() might point to a ghost cell)
+	while (not cell->is_locally_owned()) {
+		cell++;
+	}
 	SemiLagrangian<2>::Neighborhood neighbors;
 	sl.getNeighborhood(cell, neighbors);
 	dealii::Point<2> cell_center = cell->barycenter();
 	BOOST_CHECK_EQUAL(neighbors.size(), size_t(9));
-	double dx = pow(0.5, 3);
-	size_t n_periodic_x = 0;
-	size_t n_periodic_y = 0;
-	for (size_t i = 0; i < neighbors.size(); i++) {
-		dealii::Point<2> n_center = neighbors.at(i)->barycenter();
-		// periodic boundaries
-		if (n_center(0) > 0.5) {
-			n_center(0) = 1.0 - n_center(0);
-			n_periodic_x++;
+
+	// the following tests are only valid if the cell is a left upper or left lower corner
+	if (is_MPI_rank_0()) {
+		double dx = pow(0.5, 3);
+		size_t n_periodic_x = 0;
+		size_t n_periodic_y = 0;
+		for (size_t i = 0; i < neighbors.size(); i++) {
+			dealii::Point<2> n_center = neighbors.at(i)->barycenter();
+			// periodic boundaries
+			if (n_center(0) > 0.8) {
+				n_center(0) = 1.0 - n_center(0);
+				n_periodic_x++;
+			}
+			if (n_center(1) > 0.8) {
+				n_center(1) = 1.0 - n_center(1);
+				n_periodic_y++;
+			}
+			double dist = cell_center.distance(n_center);
+			// check dist \in {0, dx, sqrt(2)*dx}
+			BOOST_CHECK_LE(dist, sqrt(2) * dx + 1e-8);
+			if (dist < dx - 1e-8) {
+				BOOST_CHECK_SMALL(dist, 1e-8);
+			} else if (dist < dx + 1e-8) {
+				BOOST_CHECK_GT(dist, dx - 1e-8);
+			} else { // (dist < dx * sqrt(2) + 1e-8)
+				BOOST_CHECK_GT(dist, dx * sqrt(2) - 1e-8);
+			}
 		}
-		if (n_center(1) > 0.5) {
-			n_center(1) = 1.0 - n_center(1);
-			n_periodic_y++;
-		}
-		double dist = cell_center.distance(n_center);
-		// check dist \in {0, dx, sqrt(2)*dx}
-		BOOST_CHECK_LE(dist, sqrt(2) * dx + 1e-8);
-		if (dist < dx - 1e-8) {
-			BOOST_CHECK_SMALL(dist, 1e-8);
-		} else if (dist < dx + 1e-8) {
-			BOOST_CHECK_GT(dist, dx - 1e-8);
-		} else  { // (dist < dx * sqrt(2) + 1e-8)
-			BOOST_CHECK_GT(dist, dx * sqrt(2) - 1e-8);
-		}
+
+		// cell is corner!
+		BOOST_CHECK_EQUAL(n_periodic_x, size_t(3));
+		BOOST_CHECK_EQUAL(n_periodic_y, size_t(3));
 	}
-	// cell is corner!
-	BOOST_CHECK_EQUAL(n_periodic_x, 3);
-	BOOST_CHECK_EQUAL(n_periodic_y, 3);
 
 	pout << "done." << endl;
 } /* SemiLagrangian2D_Neighborhood_test */
@@ -97,50 +106,59 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian3D_Neighborhood_test) {
 	PeriodicTestDomain3D periodic(refinementLevel);
 	periodic.refineAndTransform();
 	SemiLagrangian<3> sl(periodic.getMesh(), periodic.getBoundaries(), fe_order,
-			boost::make_shared<D3Q19>(), 0.001 );
+			boost::make_shared<D3Q19>(), 0.001);
 	sl.setupDoFs();
 	typename dealii::DoFHandler<3>::active_cell_iterator cell =
 			sl.getDoFHandler()->begin_active();
+	// make sure cell is not in the ghost layer
+	while (not cell->is_locally_owned()) {
+		cell++;
+	}
 	SemiLagrangian<3>::Neighborhood neighbors;
 	sl.getNeighborhood(cell, neighbors);
 	dealii::Point<3> cell_center = cell->barycenter();
-	BOOST_CHECK_EQUAL(neighbors.size(), 27);
-	double dx = pow(0.5, 3);
-	size_t n_periodic_x = 0;
-	size_t n_periodic_y = 0;
-	size_t n_periodic_z = 0;
-	for (size_t i = 0; i < neighbors.size(); i++) {
-		dealii::Point<3> n_center = neighbors.at(i)->barycenter();
-		// periodic boundaries
-		if (n_center(0) > 0.5) {
-			n_center(0) = 1.0 - n_center(0);
-			n_periodic_x++;
+	BOOST_CHECK_EQUAL(neighbors.size(), size_t(27));
+
+	// the following tests are only valid if the cell is a left upper or left lower corner
+	if (is_MPI_rank_0()) {
+		double dx = pow(0.5, 3);
+		size_t n_periodic_x = 0;
+		size_t n_periodic_y = 0;
+		size_t n_periodic_z = 0;
+		for (size_t i = 0; i < neighbors.size(); i++) {
+			dealii::Point<3> n_center = neighbors.at(i)->barycenter();
+			// periodic boundaries
+			if (n_center(0) > 0.5) {
+				n_center(0) = 1.0 - n_center(0);
+				n_periodic_x++;
+			}
+			if (n_center(1) > 0.5) {
+				n_center(1) = 1.0 - n_center(1);
+				n_periodic_y++;
+			}
+			if (n_center(2) > 0.5) {
+				n_center(2) = 1.0 - n_center(2);
+				n_periodic_z++;
+			}
+			double dist = cell_center.distance(n_center);
+			// check dist \in {0, dx, sqrt(2)*dx sqrt(3)*dx}
+			BOOST_CHECK_LE(dist, sqrt(3) * dx + 1e-8);
+			if (dist < dx - 1e-8) {
+				BOOST_CHECK_SMALL(dist, 1e-8);
+			} else if (dist < dx + 1e-8) {
+				BOOST_CHECK_GT(dist, dx - 1e-8);
+			} else if (dist < dx * sqrt(2) + 1e-8) {
+				BOOST_CHECK_GT(dist, dx * sqrt(2) - 1e-8);
+			} else { // (dist < dx * sqrt(3) + 1e-8)
+				BOOST_CHECK_GT(dist, dx * sqrt(3) - 1e-8);
+			}
 		}
-		if (n_center(1) > 0.5) {
-			n_center(1) = 1.0 - n_center(1);
-			n_periodic_y++;
-		}
-		if (n_center(2) > 0.5) {
-			n_center(2) = 1.0 - n_center(2);
-			n_periodic_z++;
-		}
-		double dist = cell_center.distance(n_center);
-		// check dist \in {0, dx, sqrt(2)*dx sqrt(3)*dx}
-		BOOST_CHECK_LE(dist, sqrt(3) * dx + 1e-8);
-		if (dist < dx - 1e-8) {
-			BOOST_CHECK_SMALL(dist, 1e-8);
-		} else if (dist < dx + 1e-8) {
-			BOOST_CHECK_GT(dist, dx - 1e-8);
-		} else if (dist < dx * sqrt(2) + 1e-8) {
-			BOOST_CHECK_GT(dist, dx * sqrt(2) - 1e-8);
-		} else { // (dist < dx * sqrt(3) + 1e-8)
-			BOOST_CHECK_GT(dist, dx * sqrt(3) - 1e-8);
-		}
+
+		// cell is corner!
+		BOOST_CHECK_EQUAL(n_periodic_x, size_t(9));
+		BOOST_CHECK_EQUAL(n_periodic_y, size_t(9));
+		BOOST_CHECK_EQUAL(n_periodic_z, size_t(9));
 	}
-	// cell is corner!
-	BOOST_CHECK_EQUAL(n_periodic_x, 9);
-	BOOST_CHECK_EQUAL(n_periodic_y, 9);
-	BOOST_CHECK_EQUAL(n_periodic_z, 9);
 
 	pout << "done." << endl;
 } /* SemiLagrangian3D_Neighborhood_test */
@@ -186,11 +204,15 @@ BOOST_AUTO_TEST_CASE(SemiLagrangian3D_FindPointInNeighborhood_test) {
 	dealii::Point<3> p(1, 2, 1);
 	typename dealii::DoFHandler<3>::active_cell_iterator found_in =
 			sl.recursivelySearchInNeighborhood(p, cell);
-	BOOST_CHECK(found_in == sl.getDoFHandler()->end());
+	if (is_MPI_rank_0())
+		BOOST_CHECK(found_in == sl.getDoFHandler()->end());
 
 	dealii::Point<3> p2(1, 0.5, 0.7);
 	found_in = sl.recursivelySearchInNeighborhood(p2, cell);
-	BOOST_CHECK(found_in != sl.getDoFHandler()->end());
+	if (dealii::Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) == 1) {
+		// otherwise, p2 could belong to another process' domain
+		BOOST_CHECK(found_in != sl.getDoFHandler()->end());
+	}
 
 	pout << "done." << endl;
 } /* SemiLagrangian3D_FindPointInNeighborhood_test */
