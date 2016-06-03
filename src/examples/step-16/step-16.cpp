3ec2f71459e13610f7d66336d3128cbe413608c0
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-06-03 16:15:43 +0200

    semi-lagrangian tgv3d
    Former-commit-id: 316a7d72cb6599136a667d8e2fcefab9e7e1bc46 [formerly 70d5027b3157929f9ba0b3daa81afe7dc475f596]
    Former-commit-id: 76459f510b8b42f72c46ebbed567784f974ae924

--- src/examples/step-16/step-16.cpp
+++ src/examples/step-16/step-16.cpp
@@ -41,7 +41,7 @@ int main(int argc, char** argv) {
 
 	pout
 			<< "Usage: ./step-16 <Re=800> <refinement_level=3> <p=4> <collision-id=0 (BGK: 0, KBC: 1)> "
-					"<filter=0 (no: 0, exp: 1, new: 2> <integrator-id=1> <CFL=1.0> <stencil-id=0 (D3Q15: 0; D3Q19: 1; D3Q27: 2)>"
+					"<semi_lagrange=0> <integrator-id=1> <CFL=1.0> <stencil-id=0 (D3Q15: 0; D3Q19: 1; D3Q27: 2)>"
 			<< endl;
 
 	double Re = 800;
@@ -68,11 +68,11 @@ int main(int argc, char** argv) {
 	}
 	pout << "... Coll:  " << collision_id << endl;
 
-	size_t filter_id = 0;
+	size_t semi_lagrange = 0;
 	if (argc >= 6) {
-		filter_id = std::atoi(argv[5]);
+		semi_lagrange = std::atoi(argv[5]);
 	}
-	pout << "... Filter:  " << filter_id << endl;
+	pout << "... Filter:  " << semi_lagrange << endl;
 
 	size_t integrator_id = 1;
 	if (argc >= 7) {
@@ -135,17 +135,17 @@ int main(int argc, char** argv) {
 	// setup configuration
 	std::stringstream dirName;
 	dirName << getenv("NATRIUM_HOME") << "/step-TGV3D/Re" << Re << "-ref"
-			<< refinement_level << "-p" << p << "-coll" << collision_id << "-f"
-			<< filter_id << "-int" << integrator_id << "-CFL" << CFL << "-sten" << stencil_id;
+			<< refinement_level << "-p" << p << "-coll" << collision_id << "-sl"
+			<< semi_lagrange << "-int" << integrator_id << "-CFL" << CFL << "-sten" << stencil_id;
 	boost::shared_ptr<SolverConfiguration> configuration = boost::make_shared<
 			SolverConfiguration>();
 	//configuration->setSwitchOutputOff(true);
 	configuration->setOutputDirectory(dirName.str());
 	//configuration->setRestartAtLastCheckpoint(false);
 	configuration->setUserInteraction(false);
-	configuration->setOutputTableInterval(10);
+	configuration->setOutputTableInterval(1);
 	configuration->setOutputCheckpointInterval(10000);
-	configuration->setOutputSolutionInterval(1000);
+	configuration->setOutputSolutionInterval(100);
 	configuration->setSimulationEndTime(10.0);
 	configuration->setInitializationScheme(EQUILIBRIUM);
 	configuration->setSedgOrderOfFiniteElement(p);
@@ -158,16 +158,13 @@ int main(int argc, char** argv) {
 	}
 	//configuration->setCommandLineVerbosity(BASIC);
 	configuration->setCFL(CFL);
-	if (dt > 0.1) {
-		pout << "Timestep too big." << endl;
-	}
 	if (collision_id == 1) {
 		configuration->setCollisionScheme(KBC_STANDARD);
 	}
 	configuration->setTimeIntegrator(time_integrator);
 	configuration->setDealIntegrator(deal_integrator);
-	if (filter_id == 1) {
-		configuration->setFiltering(EXPONENTIAL_FILTER);
+	if (semi_lagrange == 1) {
+		configuration->setAdvectionScheme(SEMI_LAGRANGIAN);
 	}
 
 	//configuration->setNumberOfTimeSteps(1.0 / dt);
