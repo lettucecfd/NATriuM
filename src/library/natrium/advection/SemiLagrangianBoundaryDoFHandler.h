a6337d9f5192d5e5db501d45c4a83a0973eb960a
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-06-15 12:54:42 +0200

    Semi-Lagrangian boundary conditions: Slightly changed the structure of the boundary representation. The evaluation of the boundary uses virtual functions now -- that retains a clearer structure; but it may deterioate the performance
    Former-commit-id: 78c14a52be15b58cad17b689132aec223dcc4231 [formerly bd638f1fcd54a0a9fd9337f6afe8c39178837a36]
    Former-commit-id: b111419e9e660917a78760885b6dff0ce3e073bd

--- src/library/natrium/advection/SemiLagrangianBoundaryDoFHandler.h
+++ src/library/natrium/advection/SemiLagrangianBoundaryDoFHandler.h
@@ -11,116 +11,13 @@
 #include "deal.II/base/index_set.h"
 #include "deal.II/base/tensor.h"
 #include "deal.II/dofs/dof_handler.h"
+
+#include "BoundaryHit.h"
+#include "../problemdescription/BoundaryCollection.h"
 #include "../utilities/BasicNames.h"
 
+
 namespace natrium {
-
-/**
- * @short An enum that describes the type of the boundary.
- */
-enum BoundaryHitType {
-	LINEAR_RHO_U
-};
-
-/**
- * @short A struct that defines all information of a boundary hit. (When and where did the distribution hit the boundary hit, ...)
- */
-template<size_t dim>
-struct BoundaryHit {
-
-	/**
-	 * @short Constructor
-	 * @param coord coordinates of the point where the distribution hit the boundary
-	 * @param t_shift time shift: t_hit := t + dt - t_shift denotes the point in time when the distribution hit the boundary (where t denots the global time step)
-	 * @param n the outside unit normal vector at the boundary hit point
-	 * @param type the type of the boundary, e.g. LINEAR_RHO_U
-	 * @param c present cell
-	 * @param out_direction the unknown distribution that is to be calculated at the boundary
-	 * @note Each distribution that is required to close the streaming step needs a separate BoundaryHit instance.
-	 * @note The remaining fields incomingDirections, fIn have to filled after construction.
-	 */
-	BoundaryHit(const dealii::Point<dim>& coord, double t_shift,
-			const dealii::Tensor<1, dim>& n, BoundaryHitType type,
-			typename dealii::DoFHandler<dim>::cell_iterator& c, size_t out_direction) {
-		coordinates = coord;
-		time_shift = t_shift;
-		faceNormal = n;
-		boundaryType = type;
-		cell = c;
-		outgoingDirection = out_direction;
-		fOut = -1e20;
-	}
-
-	/**
-	 * @short copy constructor
-	 */
-	BoundaryHit (const BoundaryHit& other){
-		coordinates = other.coordinates;
-		time_shift = other.time_shift;
-		faceNormal = other.faceNormal;
-		boundaryType = other.boundaryType;
-		cell = other.cell;
-		outgoingDirection = other.outgoingDirection;
-		fOut = other.fOut;
-		incomingDirections = other.incomingDirections;
-		fIn = other.fIn;
-	}
-
-	////////////////////////
-	// global information //
-	////////////////////////
-	/**
-	 * @short coordinates of the point where the Lagrangian path hits the boundary
-	 */
-	dealii::Point<dim> coordinates;
-
-	/**
-	 * @short time shift; the point in time when the Lagrangian path hits the boundary is defined as t+dt-time_shift
-	 */
-	double time_shift;
-
-	/**
-	 * @short outward unity normal vector at the boundary point
-	 */
-	dealii::Tensor<1, dim> faceNormal;
-
-	/**
-	 * @short the type of the boundary hit point
-	 */
-	BoundaryHitType boundaryType;
-
-	/**
-	 * @short cell that contains the boundary hit point
-	 */
-	typename dealii::DoFHandler<dim>::cell_iterator cell;
-
-
-	///////////////////////////////////////
-	// outgoing distribution (only one!) //
-	///////////////////////////////////////
-	/**
-	 * @short index of outgoing direction
-	 */
-	size_t outgoingDirection;
-	/**
-	 * @short value of the outgoing distribution (has to be calculated in each time step)
-	 */
-	double fOut;
-
-	///////////////////////////
-	// incoming distribution //
-	///////////////////////////
-	/**
-	 * @short vector that contains the indices of incoming directions
-	 */
-	vector<size_t> incomingDirections;
-
-	/**
-	 * @short vector of references to the values of incoming directions
-	 */
-	vector<dealii::TrilinosWrappers::internal::VectorReference> fIn;
-
-};
 
 /**
  * @short
@@ -139,15 +36,22 @@ private:
 
 	std::vector<BoundaryHit<dim> > m_secondaryBoundaryPoints;
 
+	// boost::shared_ptr<BoundaryCollection<dim> >& m_boundaries;
+
 public:
 	SemiLagrangianBoundaryDoFHandler();
 	virtual ~SemiLagrangianBoundaryDoFHandler();
 
+	/**
+	 * @short
+	 */
 	void addBoundaryHit(BoundaryHit<dim>& boundary_hit){
 
 	}
 	void calculateBoundaryValues(double time_of_next_step){}
 	void applyBoundaryValues(distributed_block_vector& f){}
+
+
 
 };
 
