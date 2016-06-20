8824e22e82a4920c43d83b049dfbd73845912842
Dominik Wilde <dominik.wilde@gmail.com>
2016-06-20 10:17:22 +0200

    Multistep added
    Former-commit-id: 39d2e3cdbcb956d81b389d7aed528861d1f529e2 [formerly b480fdecc7e1f1d54a129423c62d9983834ecd4f]
    Former-commit-id: c097abe75c6bcb54b80fed253d8e5d97c70373bf

---
+++ src/library/natrium/collision/MultistepCollisionModel.cpp
@@ -0,0 +1,21 @@
+/*
+ * MultistepCollisionModel.cpp
+ *
+ *  Created on: 20.06.2016
+ *      Author: dominik
+ */
+
+#include <natrium/collision/MultistepCollisionModel.h>
+
+namespace natrium {
+
+MultistepCollisionModel::MultistepCollisionModel() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MultistepCollisionModel::~MultistepCollisionModel() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace natrium */
