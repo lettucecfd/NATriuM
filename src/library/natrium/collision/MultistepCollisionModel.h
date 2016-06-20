024149dd92bfb180ddf828838b22c98cae329b50
Dominik Wilde <dominik.wilde@gmail.com>
2016-06-20 10:19:03 +0200

    Multistep added
    Former-commit-id: 58432c6d1fa033081a54770a9904a5212ff24c7f [formerly 3fb3cfa2efed5f7ffcf12b70bc5ad3fa8b60ba13]
    Former-commit-id: 3218fc3caa332bb5270564e975552d0cedf0599d

--- src/library/natrium/collision/MultistepCollisionModel.h
+++ src/library/natrium/collision/MultistepCollisionModel.h
@@ -13,11 +13,40 @@
 namespace natrium {
 
 class MultistepCollisionModel: public CollisionModel {
+
+private:
+	DistributionFunctions m_formerF;
+	DistributionFunctions m_formerFEq;
+
 public:
-	MultistepCollisionModel();
+	MultistepCollisionModel(double relaxationParameter, double dt, const boost::shared_ptr<Stencil> stencil);
+
+
 	virtual ~MultistepCollisionModel();
+
+	virtual void collideAll(DistributionFunctions& f,
+				distributed_vector& densities,
+				vector<distributed_vector>& velocities,
+				const dealii::IndexSet& locally_owned_dofs,
+				bool inInitializationProcedure = false) const;
+
+	double getEquilibriumDistribution(size_t i, const numeric_vector& u,
+				const double rho) const;
+
+	DistributionFunctions getFormerF()
+	{
+		return m_formerF;
+	}
+
+	DistributionFunctions getFormerFEq()
+	{
+		return m_formerFEq;
+	}
+
+
+
 };
 
 } /* namespace natrium */
 
-#endif /* LIBRARY_NATRIUM_COLLISION_MULTISTEPCOLLISIONMODEL_H_ */
+#endif /* MULTISTEPCOLLISIONMODEL_H_ */
