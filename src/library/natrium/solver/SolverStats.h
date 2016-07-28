065fc8e0c2f46cb36e13848371b869c2c8cd5f29
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-07-28 22:01:15 +0200

    Shear layer benchmark according to Minion and Brown; Added enstrophy; Unit test for enstrophy and kinetic energy; Unit test cleaned up
    Former-commit-id: d13ecf68cb152a4cca1483612f41e2b7f2e1f90c [formerly 44806a2049f13f9ad790f6de7166a609e071c265]
    Former-commit-id: b01b2097b20efd85365a6f08f1a64d0863ac96d3

--- src/library/natrium/solver/SolverStats.h
+++ src/library/natrium/solver/SolverStats.h
@@ -35,6 +35,7 @@ private:
 	double m_time;
 	double m_maxU;
 	double m_kinE;
+	double m_enstrophy;
 	double m_maxP;
 	double m_minP;
 	double m_meanVelocityX;
@@ -54,6 +55,7 @@ public:
 		m_time = 0;
 		m_maxU = 0;
 		m_kinE = 0;
+		m_enstrophy = 0;
 		m_maxP = 0;
 		m_minP = 0;
 
@@ -81,7 +83,7 @@ public:
 		assert(not m_outputOff);
 		if (is_MPI_rank_0()) {
 			(*m_tableFile)
-					<< "#  i      t      max |u_numeric|    kinE    maxP     minP    residuum(rho)   residuum(u)    mean(ux)"
+					<< "#  i      t      max |u_numeric|    kinE  enstrophy   maxP     minP    residuum(rho)   residuum(u)    mean(ux)"
 					<< endl;
 		}
 	}
@@ -94,6 +96,7 @@ public:
 		m_maxU = m_solver->getMaxVelocityNorm();
 		m_kinE = PhysicalProperties<dim>::kineticEnergy(m_solver->getVelocity(),
 				m_solver->getDensity(), m_solver->getAdvectionOperator());
+		m_enstrophy = PhysicalProperties<dim>::enstrophy(m_solver->getVelocity(), m_solver->getAdvectionOperator());
 		m_maxP = PhysicalProperties<dim>::maximalPressure(
 				m_solver->getDensity(),
 				m_solver->getStencil()->getSpeedOfSound(), m_minP);
@@ -137,7 +140,7 @@ public:
 		}
 		if (is_MPI_rank_0()) {
 			(*m_tableFile) << m_iterationNumber << " " << m_time << " "
-					<< m_maxU << " " << m_kinE << " " << m_maxP << " " << m_minP
+					<< m_maxU << " " << m_kinE << " " << m_enstrophy << " " << m_maxP << " " << m_minP
 					<< " " << m_solver->m_residuumDensity << " "
 					<< m_solver->m_residuumVelocity << " " << m_meanVelocityX
 					<< endl;
@@ -150,6 +153,10 @@ public:
 
 	double getKinE() const {
 		return m_kinE;
+	}
+
+	double getEnstrophy() const {
+		return m_enstrophy;
 	}
 
 	double getMaxU() const {
