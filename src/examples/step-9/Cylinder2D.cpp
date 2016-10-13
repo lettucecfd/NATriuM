74a73a363fb58121ce1917fc3bca258c9ff7cd8f
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-10-13 13:56:05 +0200

    Cylinder benchmark: Build grid through deal.II grid generating functions
    Former-commit-id: 6057b083637440baaed2bf9c2d9986ad2fcc215f [formerly b45f7552885eb5726968ee116bcc551a3963e7e8]
    Former-commit-id: 8cc67ec0a37bb59e940740f01d72942a6195e9cc

--- src/examples/step-9/Cylinder2D.cpp
+++ src/examples/step-9/Cylinder2D.cpp
@@ -7,7 +7,11 @@
 
 #include "Cylinder2D.h"
 
+#include <fstream>
+
+#include "deal.II/grid/grid_generator.h"
 #include "deal.II/grid/grid_in.h"
+#include "deal.II/grid/grid_out.h"
 #include "deal.II/grid/grid_tools.h"
 #include "deal.II/grid/tria_accessor.h"
 #include "deal.II/grid/tria_iterator.h"
@@ -16,13 +20,14 @@
 #include "natrium/boundaries/PeriodicBoundary.h"
 #include "natrium/boundaries/LinearFluxBoundaryRhoU.h"
 #include "natrium/utilities/CFDSolverUtilities.h"
+#include "natrium/utilities/DealiiExtensions.h"
 #include "natrium/utilities/Logging.h"
 
 namespace natrium {
 
-
 Cylinder2D::Cylinder2D(double viscosity, double inletVelocity) :
-		ProblemDescription<2>(makeGrid(), viscosity, 1.0), m_inletVelocity(inletVelocity) {
+		ProblemDescription<2>(makeGrid(), viscosity, 1.0), m_inletVelocity(
+				inletVelocity) {
 	setCharacteristicLength(1.0);
 
 	/// apply boundary values
@@ -32,39 +37,168 @@ Cylinder2D::Cylinder2D(double viscosity, double inletVelocity) :
 Cylinder2D::~Cylinder2D() {
 }
 
+void Cylinder2D::make_inner_manifold(dealii::Triangulation<2>& mesh,
+		const dealii::SphericalManifold<2>& manifold, double radius, int boundary_id,
+		double tol) {
+
+	dealii::Triangulation<2>::active_cell_iterator cell = mesh.begin_active(),
+			endc = mesh.end();
+	for (; cell != endc; ++cell) {
+		for (unsigned int f = 0; f < dealii::GeometryInfo<2>::faces_per_cell;
+				++f) {
+			bool is_inner_face = true;
+			for (unsigned int v = 0;
+					v < dealii::GeometryInfo<2>::vertices_per_face; ++v) {
+				const double distance_from_center = manifold.center.distance(
+						cell->face(f)->vertex(v));
+				if (distance_from_center >=  radius + tol) {
+					is_inner_face = false;
+					break;
+				}
+			}
+			if (is_inner_face) {
+				//cout << cell->face(f)->barycenter() << endl;
+				cell->face(f)->set_manifold_id(1);
+				if (boundary_id > 0){
+					cell->face(f)->set_boundary_id(boundary_id);
+				}
+			}
+		}
+	}
+	mesh.set_manifold(1, manifold);
+
+}
+
 boost::shared_ptr<Mesh<2> > Cylinder2D::makeGrid() {
 
-	//Creation of the principal domain
-	boost::shared_ptr<Mesh<2> > tria = boost::make_shared<Mesh<2> >();
-	dealii::GridIn<2> gridin;
-	gridin.attach_triangulation(*tria);
-	// !!! When including unv meshes from Salome, the first two blocks have to be deleted manually (compare Original_Mesh_1.unv to Mesh_1.unv)
-	// The mesh file then regularly begins with the lines "-1" and "2411"
-	std::ifstream f("/home/kraemer/eclipse_workspace/NATriuM/src/examples/step-9/salome/Mesh_1.unv");
-	gridin.read_unv(f);
-	CFDSolverUtilities::mesh_info(*tria, "cylinder.eps");
+// this is not the domain by Min and Lee! they had D=1; [-19,50]x[-25,25]
+// differences:
+// - boundaries are closer (can be fixed with a few additional rectangles)
+// - unsymmetric to make a vortex street
+// ====> [-19,50]x[-19,25]
+//Creation of the principal domain
+	const dealii::SphericalManifold<2> manifold1(dealii::Point<2>(0, 0));
+	const dealii::SphericalManifold<2> manifold2(dealii::Point<2>(0, 0));
+	const dealii::SphericalManifold<2> manifold3(dealii::Point<2>(0, 0));
 
-	return tria;
+	dealii::Triangulation<2> mesh;
+	dealii::Triangulation<2> rect;
+	dealii::Triangulation<2> inner;
+	dealii::Triangulation<2> trans;
+	dealii::Triangulation<2> tmp;
+	dealii::Triangulation<2> merge;
+	dealii::GridGenerator::hyper_cube_with_cylindrical_hole(tmp, 5, 19);
+
+// make circular
+	make_inner_manifold(tmp, manifold1, 5);
+	tmp.refine_global(2);
+	tmp.set_manifold(1);
+// delete coarse cells (Mesh.merge() requires "coarse" triangulations as input)
+	dealii::GridGenerator::flatten_triangulation(tmp, trans);
+// inner part
+	tmp.clear();
+	dealii::GridGenerator::hyper_shell(tmp, dealii::Point<2>(0,0), 0.5, 5, 8);
+	tmp.set_all_manifold_ids(1);
+	tmp.set_manifold(1, manifold2);
+	tmp.refine_global(2);
+	tmp.set_manifold(1);
+	dealii::GridGenerator::flatten_triangulation(tmp, inner);
+	dealii::GridGenerator::merge_triangulations(trans, inner, merge);
+	mesh.clear();
+	mesh.copy_triangulation(merge);
+// tail part
+	std::vector<unsigned int> repetitions;
+	repetitions.push_back(3);
+	repetitions.push_back(8);
+	dealii::GridGenerator::subdivided_hyper_rectangle(rect, repetitions,
+			dealii::Point<2>(19, -19), dealii::Point<2>(50, 19));
+	dealii::GridGenerator::merge_triangulations(mesh, rect, merge);
+	mesh.clear();
+	mesh.copy_triangulation(merge);
+// add unsymmetric part
+// rect left
+	repetitions.at(0) = 8;
+	repetitions.at(1) = 1;
+	rect.clear();
+	dealii::GridGenerator::subdivided_hyper_rectangle(rect, repetitions,
+			dealii::Point<2>(-19, 19), dealii::Point<2>(19, 25));
+	dealii::GridGenerator::merge_triangulations(mesh, rect, merge);
+	mesh.clear();
+	mesh.copy_triangulation(merge);
+// rect right 1
+	repetitions.at(0) = 3;
+	repetitions.at(1) = 1;
+	rect.clear();
+	dealii::GridGenerator::subdivided_hyper_rectangle(rect, repetitions,
+			dealii::Point<2>(19, 19), dealii::Point<2>(50, 25));
+	dealii::GridGenerator::merge_triangulations(mesh, rect, merge);
+	mesh.clear();
+	mesh.copy_triangulation(merge);
+
+// set boundary ids
+	DealIIExtensions::set_boundary_ids_at_hyperplane<2>(mesh, 0, -19, 1); // left
+	DealIIExtensions::set_boundary_ids_at_hyperplane<2>(mesh, 0, 50, 2);
+	DealIIExtensions::set_boundary_ids_at_hyperplane<2>(mesh, 1, -19, 3);
+	DealIIExtensions::set_boundary_ids_at_hyperplane<2>(mesh, 1, 25, 4);
+	make_inner_manifold(mesh, manifold3, 0.5, 0);
+	mesh.set_manifold(1);
+
+	std::stringstream s;
+	s << "/tmp/grid_cylinder.vtk";
+	cout << s.str() << endl;
+	std::ofstream out(s.str());
+	dealii::GridOut grid_out;
+	grid_out.write_vtk(mesh, out);
+	out.close();
+
+	/*
+	 dealii::GridIn<2> gridin;
+	 gridin.attach_triangulation(*tria);
+	 // !!! When including unv meshes from Salome, the first two blocks have to be deleted manually (compare Original_Mesh_1.unv to Mesh_1.unv)
+	 // The mesh file then regularly begins with the lines "-1" and "2411"
+	 std::stringstream s;
+	 s << getenv("NATRIUM_DIR") << "/src/examples/step-9/salome/Mesh_1.unv";
+	 std::ifstream f(s.str().c_str());
+	 gridin.read_unv(f);
+	 CFDSolverUtilities::mesh_info(*tria, "cylinder.eps");
+	 */
+
+	boost::shared_ptr<Mesh<2> > distributed_mesh = boost::make_shared<Mesh<2> >(
+	MPI_COMM_WORLD);
+	distributed_mesh->copy_triangulation(mesh);
+
+// release manifolds
+	tmp.set_manifold(0);
+
+	return distributed_mesh;
 }
 
 boost::shared_ptr<BoundaryCollection<2> > Cylinder2D::makeBoundaries(
 		double inletVelocity) {
 
-	// make boundary description
+// make boundary description
 	boost::shared_ptr<BoundaryCollection<2> > boundaries = boost::make_shared<
 			BoundaryCollection<2> >();
 	numeric_vector zeroVelocity(2);
 	numeric_vector constantVelocity(2);
 	constantVelocity(0) = inletVelocity;
 
-	boundaries->addBoundary(boost::make_shared<LinearFluxBoundaryRhoU<2> >(0, constantVelocity));
-	boundaries->addBoundary(boost::make_shared<LinearFluxBoundaryRhoU<2> >(1, constantVelocity));
-	boundaries->addBoundary(boost::make_shared<LinearFluxBoundaryRhoU<2> >(2, constantVelocity));
 	boundaries->addBoundary(
-			boost::make_shared<LinearFluxBoundaryRhoU<2> >(3, constantVelocity));
-	boundaries->addBoundary(boost::make_shared<LinearFluxBoundaryRhoU<2> >(4, zeroVelocity));
+			boost::make_shared<LinearFluxBoundaryRhoU<2> >(4,
+					constantVelocity));
+	boundaries->addBoundary(
+			boost::make_shared<LinearFluxBoundaryRhoU<2> >(1,
+					constantVelocity));
+	boundaries->addBoundary(
+			boost::make_shared<LinearFluxBoundaryRhoU<2> >(2,
+					constantVelocity));
+	boundaries->addBoundary(
+			boost::make_shared<LinearFluxBoundaryRhoU<2> >(3,
+					constantVelocity));
+	boundaries->addBoundary(
+			boost::make_shared<LinearFluxBoundaryRhoU<2> >(0, zeroVelocity));
 
-	// Get the triangulation object (which belongs to the parent class).
+// Get the triangulation object (which belongs to the parent class).
 	boost::shared_ptr<Mesh<2> > tria_pointer = getMesh();
 
 	return boundaries;
@@ -75,9 +209,9 @@ boost::shared_ptr<BoundaryCollection<2> > Cylinder2D::makeBoundaries(
  * @param[out] initialDensities vector of densities; to be filled
  * @param[in] supportPoints the coordinates associated with each degree of freedom
  */
-void  Cylinder2D::applyInitialDensities(distributed_vector& initialDensities,
-		const vector<dealii::Point<2> >& supportPoints) const{
-	for (size_t i = 0; i < initialDensities.size(); i++){
+void Cylinder2D::applyInitialDensities(distributed_vector& initialDensities,
+		const vector<dealii::Point<2> >& supportPoints) const {
+	for (size_t i = 0; i < initialDensities.size(); i++) {
 		initialDensities(i) = 1.0;
 	}
 }
@@ -87,15 +221,14 @@ void  Cylinder2D::applyInitialDensities(distributed_vector& initialDensities,
  * @param[out] initialVelocities vector of velocities; to be filled
  * @param[in] supportPoints the coordinates associated with each degree of freedom
  */
- void  Cylinder2D::applyInitialVelocities(
+void Cylinder2D::applyInitialVelocities(
 		vector<distributed_vector>& initialVelocities,
-		const vector<dealii::Point<2> >& supportPoints) const{
-	 assert ( initialVelocities.size() == 2);
-		for (size_t i = 0; i < initialVelocities.at(0).size(); i++){
-			initialVelocities.at(0)(i) = getCharacteristicVelocity();
-			initialVelocities.at(1)(i) = 0.0;
-		}
- }
-
+		const vector<dealii::Point<2> >& supportPoints) const {
+	assert(initialVelocities.size() == 2);
+	for (size_t i = 0; i < initialVelocities.at(0).size(); i++) {
+		initialVelocities.at(0)(i) = getCharacteristicVelocity();
+		initialVelocities.at(1)(i) = 0.0;
+	}
+}
 
 } /* namespace natrium */
--- src/examples/step-9/Cylinder2D.h
+++ src/examples/step-9/Cylinder2D.h
@@ -10,15 +10,15 @@
 
 #include "deal.II/grid/tria.h"
 
+#include <deal.II/grid/manifold_lib.h>
+
 #include "natrium/problemdescription/ProblemDescription.h"
 #include "natrium/utilities/BasicNames.h"
 
-
-
 namespace natrium {
 
-/** @short Description of the flow around a circular cylinder (regular channel flow in square domain).
- *
+/**
+ * @short Description of the flow around a circular cylinder (regular channel flow in square domain).
  */
 class Cylinder2D: public ProblemDescription<2> {
 public:
@@ -34,16 +34,29 @@ public:
 	}
 
 	virtual void refine(Mesh<2>& mesh) {
-
+		// remember to make_inner_manifold when implementing global refinement
 	}
 
-	virtual void transform(Mesh<2>& mesh){
+	virtual void transform(Mesh<2>& mesh) {
 
 	}
 
 private:
 
 	const double m_inletVelocity;
+
+	/**
+	 * @ short Sets the manifold id and boundary id of the inner manifold, respectively
+	 * @ short The manifold will contain all faces that have a distance <= radius + tol to the center
+	 * @param[in] mesh the mesh
+	 * @param[in] manifold the spherical manifold (that also contains the center)
+	 * @param[in] radius distance from center to sphere (default: 0.5)
+	 * @param[in] boundary_id: if >= 0, then all faces in the manifold get this boundary id
+	 * @param[in] tol tolerance for checking the distance (default: 1e-10)
+	 */
+	static void make_inner_manifold(dealii::Triangulation<2>& mesh,
+			const dealii::SphericalManifold<2>& manifold, double radius = 0.5, int boundary_id = -1,
+			double tol = 1e-10);
 
 	/**
 	 * @short create triangulation for cylinder flow
@@ -56,7 +69,8 @@ private:
 	 * @return shared pointer to a vector of boundaries
 	 * @note All boundary types are inherited of BoundaryDescription; e.g. PeriodicBoundary
 	 */
-	boost::shared_ptr<BoundaryCollection<2> > makeBoundaries(double inletVelocity);
+	boost::shared_ptr<BoundaryCollection<2> > makeBoundaries(
+			double inletVelocity);
 
 	/**
 	 * @short set initial densities
@@ -75,9 +89,7 @@ private:
 			vector<distributed_vector>& initialVelocities,
 			const vector<dealii::Point<2> >& supportPoints) const;
 
-
 };
-
 
 } /* namespace natrium */
 #endif /* CYLINDER2D_H_ */
