2df50d6cb17fa3a85f38f0ffbc2f37c857f4bee4
akraem3m <akraem3m@wr-vm0.wr.inf.h-brs.de>
2016-11-28 17:19:37 +0100

    Parallel Unit test debugged.
    Former-commit-id: 3cbe895f0fbcc544fc972aac2a8b6383ab9d601c [formerly f13682fcdfa9ccfad9bbe875ffda2e526b48b9aa]
    Former-commit-id: 044459f4b773e1a86e4a5c36bf79eb54343cdbe7

---
+++ src/library/natrium/boundaries/SLEquilibriumBoundary.h
@@ -0,0 +1,21 @@
+/*
+ * SLEquilibriumBoundary.h
+ *
+ *  Created on: 28.11.2016
+ *      Author: akraem3m
+ */
+
+#ifndef LIBRARY_NATRIUM_BOUNDARIES_SLEQUILIBRIUMBOUNDARY_H_
+#define LIBRARY_NATRIUM_BOUNDARIES_SLEQUILIBRIUMBOUNDARY_H_
+
+namespace natrium {
+
+class SLEquilibriumBoundary {
+public:
+	SLEquilibriumBoundary();
+	virtual ~SLEquilibriumBoundary();
+};
+
+} /* namespace natrium */
+
+#endif /* LIBRARY_NATRIUM_BOUNDARIES_SLEQUILIBRIUMBOUNDARY_H_ */
