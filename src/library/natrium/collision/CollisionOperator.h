2116e62a3bd9c333a5ebf63958818e5465bba363
dominik <dominik.wilde@gmail.com>
2017-01-06 15:51:45 +0100

    First changes for collision_refactoring in CFDSolver.cpp
    Former-commit-id: f9aaf38951ce8991c4e9e2e94877742323785680 [formerly 6bc6345e9c2d224bed54259a754f5faa8608be82]
    Former-commit-id: a4cbb9faff0ca265955bbf55d4bdc9860b71f1a1

---
+++ src/library/natrium/collision/CollisionOperator.h
@@ -0,0 +1,29 @@
+/*
+ * CollisionOperator.h
+ *
+ *  Created on: 06.01.2017
+ *      Author: dominik
+ */
+
+#ifndef COLLISIONOPERATOR_H_
+#define COLLISIONOPERATOR_H_
+
+namespace natrium {
+
+class CollisionOperator {
+public:
+	CollisionOperator(boost::shared_ptr<SolverConfiguration>& configuration);
+	virtual ~CollisionOperator();
+
+	void collide(boost::shared_ptr<SolverConfiguration>& configuration, DistributionFunctions& f,
+			distributed_vector& densities,
+			vector<distributed_vector>& velocities,
+			const dealii::IndexSet& locally_owned_dofs,
+			bool inInitializationProcedure = false) const;
+
+
+};
+
+} /* namespace natrium */
+
+#endif /* LIBRARY_NATRIUM_COLLISION_COLLISIONOPERATOR_H_ */
