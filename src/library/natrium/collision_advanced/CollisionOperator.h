146371c9cbd239b0d176b14b4ea5f820ca25b6d1
Dominik Wilde <dominik.wilde@gmail.com>
2017-02-02 19:07:22 +0100

    New version for collision_refactoring
    Former-commit-id: a6019f1f55a477019005cf948fabfcf8ffd30e1d [formerly 6a43597ffd788735d5bd6e22efd241b4ce7032ff]
    Former-commit-id: afc0010a57ca09c771147366c4b8b62e693060ba

--- src/library/natrium/collision_advanced/CollisionOperator.h
+++ src/library/natrium/collision_advanced/CollisionOperator.h
@@ -8,44 +8,37 @@
 #ifndef COLLISIONOPERATOR_H_
 #define COLLISIONOPERATOR_H_
 
+#include <vector>
 #include "../stencils/Stencil.h"
 #include "AuxiliaryCollisionFunctions.h"
-#include <vector>
 #include "Equilibria.h"
 #include "CollisionSchemes.h"
 
 
-
 namespace natrium {
 
-
-
-
-template<int T_D, int T_Q, template <int T_D, int T_Q> class T_equilibrium, template <int T_D, int T_Q, template <int T_D, int T_Q> class T_equilibrium> class T_collision>
+template<int T_D, int T_Q, template<int T_D, int T_Q> class T_equilibrium,
+		template<int T_D, int T_Q,
+				template<int T_D, int T_Q> class T_equilibrium> class T_collision>
 class CollisionOperator {
 public:
-/*
-	void collide(boost::shared_ptr<SolverConfiguration>& configuration, DistributionFunctions& f,
-			distributed_vector& densities,
+
+	void collideAll(DistributionFunctions& f, distributed_vector& densities,
 			vector<distributed_vector>& velocities,
 			const dealii::IndexSet& locally_owned_dofs,
-			bool inInitializationProcedure = false) const; */
-
-
-	void collideAll(DistributionFunctions& f,
-			distributed_vector& densities, vector<distributed_vector>& velocities,
-			const dealii::IndexSet& locally_owned_dofs,
-			bool inInitializationProcedure) const {
+			bool inInitializationProcedure, CollisionParameters<T_D, T_Q> params) const {
 		//for all degrees of freedom on current processor
 		dealii::IndexSet::ElementIterator it(locally_owned_dofs.begin());
 		dealii::IndexSet::ElementIterator end(locally_owned_dofs.end());
 
+	//	CollisionParameters<T_D, T_Q> params;
+
 		for (it = locally_owned_dofs.begin(); it != end; it++) {
 			size_t i = *it;
 
-			CollisionParameters<T_D,T_Q> params;
+
 			params.scaling = 1.0; // TODO adjust to global scaling
-			params.tau = 1.0 // TODO
+			params.tau = 1.0; // TODO
 			double fLocal[T_Q];
 
 			copyGlobalToLocalF<T_Q>(fLocal, f, i); // done
@@ -67,9 +60,10 @@ public:
 			T_collision<T_D, T_Q, T_equilibrium> collisionScheme;
 			collisionScheme.relax(fLocal, params);  //TODO
 
-			copyLocalToGlobalF<T_Q>(fLocal,f,i);
-
 			//reApplyForces<T_Q>(fLocal); // TODO
+
+			//copyLocalToGlobalF<T_Q>(fLocal, f, i); //TODO
+
 
 
 		}
@@ -77,29 +71,6 @@ public:
 	}
 };
 
-
-inline void selectCollide(
-		boost::shared_ptr<SolverConfiguration>& configuration,
-		DistributionFunctions& f, distributed_vector& densities,
-		vector<distributed_vector>& velocities,
-		const dealii::IndexSet& locally_owned_dofs,
-		bool inInitializationProcedure) {
-
-	switch (configuration->getStencil()) {
-	case Stencil_D2Q9:
-		switch (configuration->getCollisionScheme()) {
-		case BGK_STANDARD:
-			switch (configuration->getEquilibriumScheme()) {
-			case BGK_EQUILIBRIUM:
-				cout << "ok";
-
-			CollisionOperator<2,9,BGKEquilibrium, BGKCollision> test;
-			test.collideAll (f, densities,velocities, locally_owned_dofs, inInitializationProcedure);
-			}
-		}
-
-	}
-}
 
 
 } /* namespace natrium */
