82d756bc8500cabb547132a88568b8b17ba37eeb
Dominik Wilde <dominik.wilde@h-brs.de>
2019-09-03 09:05:06 +0200

    Changes in Riemann
    Former-commit-id: d11b481a8e9a6d2f1c80cb0f5a7fa195987a417a [formerly 2f51660775ab9cfeb27e827888049faebe8907d3]
    Former-commit-id: 6ed98ba70c246c03fd2c7c17c24a58df4c501689

--- src/examples/step-11/riemann.cpp
+++ src/examples/step-11/riemann.cpp
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     double t_max=1.0;
 
     double scaling = 1.0;
-    double viscosity = 0.0000001;
+    double viscosity = 0.001;
 
 	boost::shared_ptr<ProblemDescription<2> > riemann = boost::make_shared<
             Riemann2D>(viscosity, parser.getArgument<int>("ref-level"), u0,
