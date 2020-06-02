147cbd7fd4cf631446b7809afc6cdef413c9ec25
Dominik Wilde <dominik.wilde@gmail.com>
2020-06-02 13:04:45 +0200

    D2Q19 in seperate files
    Former-commit-id: c561f9d8a2bf78cdb54f4c237c246d5ad054338d [formerly 3d53032cd5dc858350359eb8aabfb4421e1c968d]
    Former-commit-id: faf82a85a7b7b0165afdc08248860a09031cfa3d
    Former-commit-id: d107e8394d0e972c1f47be02abcb9307650bfbaa

---
+++ src/library/natrium/stencils/D2Q19.cpp
@@ -0,0 +1,135 @@
+/**
+ * @file D2Q25H.cpp
+ * @short 
+ * @date 09.10.2018
+ * @author dwilde3m, Bonn-Rhein-Sieg University of Applied Sciences, Sankt Augustin
+ */
+
+#include "D2Q25H.h"
+
+#include <math.h>
+
+// enable + operator for filling vectors
+#include "boost/assign/std/vector.hpp"
+
+// enable + operator for filling vectors
+using namespace boost::assign;
+
+namespace natrium {
+
+/////////////////////////////
+// ASSIGN STATIC VARIABLES //
+/////////////////////////////
+
+// assign D and Q
+// has to be done outside the class, because function calls are not allowed in initialization of statics
+/// D
+const size_t D2Q25H::D = 2;
+/// Q
+const size_t D2Q25H::Q = 19;
+
+/// constructor
+D2Q25H::D2Q25H(double scaling) :
+		Stencil(2, 19, makeDirections(scaling), makeWeights(), Stencil_D2Q25H,
+				makeMomentBasis(makeDirections(scaling))), m_speedOfSound(
+                scaling/sqrt(3)), m_speedOfSoundSquare(
+                scaling * scaling/(3)), m_scaling(scaling) {
+} //constructor
+
+/// destructor
+D2Q25H::~D2Q25H() {
+} /// destructor
+
+
+
+// make weights
+vector<double> D2Q25H::makeWeights() {
+    double r = (sqrt(5.)-sqrt(2.))/sqrt(3.);
+
+	double w_0 = (-3-3*r*r*r*r+54*r*r)/(75*r*r);
+
+	double w_m = (9*r*r*r*r-6-27*r*r)/(300*r*r*(r*r-1));
+	double w_n = (9-6*r*r*r*r-27*r*r)/(300*(1-r*r));
+
+
+	double w_0n = w_0*w_n;
+	double w_0m = w_0*w_m;
+	double w_mm = w_m*w_m;
+	double w_mn = w_m*w_n;
+	double w_nn = w_n*w_n;
+	vector<double> result { 0.3168437267921905 , 0.1024247123210936 ,
+                           0.1024247123210936 ,
+                           0.1024247123210936 ,
+                           0.1024247123210936 ,
+                           0.002405335328939458 ,
+                           0.002405335328939458 ,
+                           0.002405335328939458 ,
+                           0.002405335328939458 ,
+                           0.00953510698543825 ,
+                           0.00953510698543825 ,
+                           0.00953510698543825 ,
+                           0.00953510698543825 ,
+                           0.006865104210104631 ,
+                           0.006865104210104631 ,
+                           0.10558878375062891 ,
+                           0.10558878375062891 ,
+                           0.0003939393722285871 ,
+                           0.0003939393722285871
+                          };
+	//result += w_0*w_0, w_0m,w_0m, w_0m,w_0m, w_mm, w_mm, w_mm, w_mm, w_0n, w_0n, w_0n, w_0n, w_nn, w_nn, w_nn, w_nn, w_mn,w_mn,w_mn,w_mn,w_mn,w_mn,w_mn,w_mn ;
+	return result;
+} /// make weights
+
+/// make directions
+vector<numeric_vector> D2Q25H::makeDirections(double scaling) {
+    const double c_m = scaling*sqrt(5.-sqrt(10.))/sqrt(3.);
+    const double c_n = sqrt(5.+sqrt(10.))*scaling/sqrt(3.);
+	const double directionsArray[][2] = {{ 0.0 , 0.0 },{ 1.367469636752619 , 0.775196278121181 },
+        { 1.367469636752619 , -0.775196278121181 },
+        { -1.367469636752619 , 0.775196278121181 },
+        { -1.367469636752619 , -0.775196278121181 },
+        { 2.6987507639352253 , 1.8663975507141328 },
+        { 2.6987507639352253 , -1.8663975507141328 },
+        { -2.6987507639352253 , 1.8663975507141328 },
+        { -2.6987507639352253 , -1.8663975507141328 },
+        { 1.105629214668943 , 2.5175897644357486 },
+        { 1.105629214668943 , -2.5175897644357486 },
+        { -1.105629214668943 , 2.5175897644357486 },
+        { -1.105629214668943 , -2.5175897644357486 },
+        { 2.9213306655318734 , 0.0 },
+        { -2.9213306655318734 , 0.0 },
+        { 0.0 , 1.4869982213169028 },
+        { 0.0 , -1.4869982213169028 },
+        { 0.0 , 3.8358342053914734 },
+        { 0.0 , -3.8358342053914734 }};
+
+
+        vector<numeric_vector> result;
+	for (size_t i = 0; i < Q; i++) {
+		numeric_vector direction(2);
+		direction(0) = directionsArray[i][0] / sqrt(3);
+		direction(1) = directionsArray[i][1] / sqrt(3);
+		result += direction;
+	}
+	return result;
+} /// make directions
+
+    numeric_matrix D2Q25H::makeMomentBasis(vector<numeric_vector> e) {
+        numeric_matrix m(Q);
+        for (int i = 0;i<Q;i++){
+            for (int j = 0;j<Q;j++){
+                if(i==j)
+                {
+                    m(i,j) =1.0;
+                } else
+                {
+                    m(i,j) =0.0;
+                }
+            }
+
+        }
+        return m;
+    }
+
+} /* namespace natrium */
+
