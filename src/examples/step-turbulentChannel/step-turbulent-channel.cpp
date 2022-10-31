4ba61d8be293f403a605e5af1a296135a2bbf0ae
Wilde <dw872713@hpc-login02.cm.cluster>
2022-10-31 16:01:12 +0100

    Changes

--- src/examples/step-turbulentChannel/step-turbulent-channel.cpp
+++ src/examples/step-turbulentChannel/step-turbulent-channel.cpp
@@ -127,7 +127,7 @@ int main(int argc, char** argv) {
 	const double u_cl = parser.getArgument<double>("U_cl");
 	const double Ma = parser.getArgument<double>("Ma");
 	const double gridDensity = parser.getArgument<double>("grid-density");
-	const double scaling = sqrt(3) * u_cl / (0.85*Ma*sqrt(gamma));
+	const double scaling = sqrt(3) * u_cl / (Ma*sqrt(gamma*reference_temperature));
 
 	const double viscosity = u_cl * delta / parser.getArgument<double>("Re_cl");
 	const double utau = Re_tau * viscosity / delta;
