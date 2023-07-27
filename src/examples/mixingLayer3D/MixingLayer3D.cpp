47e7a24b1efc89369bc2bc35a67c10c518df359c
PhiSpel <philipp.spelten@web.de>
2023-07-27 16:25:09 +0200

    stronger disturbance

--- src/examples/mixingLayer3D/MixingLayer3D.cpp
+++ src/examples/mixingLayer3D/MixingLayer3D.cpp
@@ -43,7 +43,7 @@ MixingLayer3D::~MixingLayer3D() = default;
 double MixingLayer3D::InitialVelocity::value(const dealii::Point<3>& x, const unsigned int component) const {
     assert(component < 3);
     double rand_u = InterpolateVelocities(x(0), x(1), x(2), component);
-    rand_u *= exp(-pow((x(1))/(2 * shearlayerthickness), 2));
+    rand_u *= exp(-pow((x(1))/(2 * shearlayerthickness), 2)) * 10;
     if (component == 0) {
         return tanh(-x(1)/(2 * shearlayerthickness)) + rand_u;
     } else {
