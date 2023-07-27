d473a78df9d4eb8f3135a862fa2a56ebb6fbb700
PhiSpel <philipp.spelten@web.de>
2023-07-27 12:01:13 +0200

    reduce points for initial velocity

--- src/examples/mixingLayer3D/MixingLayer3D.cpp
+++ src/examples/mixingLayer3D/MixingLayer3D.cpp
@@ -30,7 +30,7 @@ double shearlayerthickness = 0.093;
 int n = 3;
 int kmax = pow(2, n); // [1] C. Pantano and S. Sarkar, “A study of compressibility effects in the high-speed turbulent shear layer using direct simulation,” J. Fluid Mech., vol. 451, pp. 329–371, Jan. 2002, doi: 10.1017/S0022112001006978.
 // kmax = 32
-int npoints = 64; // number of points in shortest axis of velocity field
+int npoints = 5; // number of points in shortest axis of velocity field
 
 namespace natrium {
 
