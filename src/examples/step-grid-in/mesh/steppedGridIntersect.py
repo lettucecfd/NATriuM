f325ed26c2a7828e723038426dc041080539e08b
PhiSpel <philipp.spelten@web.de>
2025-06-25 16:47:37 +0200

    stepped update

--- src/examples/step-grid-in/mesh/steppedGridIntersect.py
+++ src/examples/step-grid-in/mesh/steppedGridIntersect.py
@@ -8,31 +8,33 @@ gmsh.initialize()
 gmsh.model.add("clipped_cartesian")
 
 # Parameters
-Lx = 9
-Ly = 7
+Lx = 7
+Ly = 4
 xmin = -1.5
 ymin = -Ly/2
 xmax = xmin + Lx
 ymax = ymin + Ly
-dx_foil = 0.001
-dx_around = 0.35
-prog_y = 1.3
-prog_x = 1.2
+dx_foil = 0.002
+dx_around = 0.05
+prog_y = 1.05
+prog_x_in = 1.2
+prog_x_out = 1.03
 
 # foil coordinates
 x = [1.0, 0.999748, 0.998993, 0.997736, 0.995977, 0.993719, 0.990964, 0.987715, 0.983974, 0.979746, 0.975036, 0.969846, 0.964184, 0.958054, 0.951463, 0.944418, 0.936925, 0.928992, 0.920627, 0.911838, 0.902635, 0.893027, 0.883022, 0.872632, 0.861867, 0.850737, 0.839255, 0.82743, 0.815276, 0.802805, 0.790028, 0.77696, 0.763613, 0.75, 0.736136, 0.722033, 0.707708, 0.693173, 0.678443, 0.663534, 0.64846, 0.633237, 0.617879, 0.602403, 0.586824, 0.571157, 0.555419, 0.539625, 0.523791, 0.507933, 0.492067, 0.476209, 0.460375, 0.444581, 0.428843, 0.413176, 0.397597, 0.382121, 0.366763, 0.35154, 0.336466, 0.321557, 0.292292, 0.277967, 0.263864, 0.25, 0.236387, 0.22304, 0.209972, 0.197195, 0.184724, 0.17257, 0.160745, 0.149263, 0.138133, 0.127368, 0.116978, 0.106973, 0.0973649, 0.0881617, 0.0793732, 0.0710083, 0.0630753, 0.0555823, 0.0485367, 0.0419458, 0.035816, 0.0301537, 0.0249644, 0.0202535, 0.0160256, 0.0122851, 0.00903565, 0.00628056, 0.00402259, 0.00226404, 0.00100666, 0.000251729, 0.0, 0.000251729, 0.00100666, 0.00226404, 0.00402259, 0.00628056, 0.00903565, 0.0122851, 0.0160256, 0.0202535, 0.0249644, 0.0301537, 0.035816, 0.0419458, 0.0485367, 0.0555823, 0.0630753, 0.0710083, 0.0793732, 0.0881617, 0.0973649, 0.106973, 0.116978, 0.127368, 0.138133, 0.149263, 0.160745, 0.17257, 0.184724, 0.197195, 0.209972, 0.22304, 0.236387, 0.25, 0.263864, 0.277967, 0.306827, 0.321557, 0.336466, 0.35154, 0.366763, 0.382121, 0.397597, 0.413176, 0.428843, 0.444581, 0.460375, 0.476209, 0.492067, 0.507933, 0.523791, 0.539625, 0.555419, 0.571157, 0.586824, 0.602403, 0.617879, 0.633237, 0.64846, 0.663534, 0.678443, 0.693173, 0.707708, 0.722033, 0.736136, 0.75, 0.763613, 0.77696, 0.790028, 0.802805, 0.815276, 0.82743, 0.839255, 0.850737, 0.861867, 0.872632, 0.883022, 0.893027, 0.902635, 0.911838, 0.920627, 0.928992, 0.936925, 0.944418, 0.951463, 0.958054, 0.964184, 0.969846, 0.975036, 0.979746, 0.983974, 0.987715, 0.990964, 0.993719, 0.995977, 0.997736, 0.998993, 0.999748];
 y = [-1.66533e-17, 3.65828e-05, 0.000146223, 0.000328595, 0.00058316, 0.00090917, 0.00130567, 0.00177151, 0.00230534, 0.00290565, 0.00357073, 0.00429874, 0.00508767, 0.00593537, 0.00683958, 0.00779793, 0.00880793, 0.00986703, 0.0109726, 0.0121219, 0.0133121, 0.0145406, 0.0158044, 0.0171006, 0.0184265, 0.0197789, 0.0211551, 0.0225521, 0.0239669, 0.0253966, 0.0268382, 0.0282887, 0.0297451, 0.0312044, 0.0326635, 0.0341194, 0.035569, 0.0370091, 0.0384366, 0.0398482, 0.0412407, 0.0426107, 0.0439549, 0.0452698, 0.0465521, 0.0477982, 0.0490045, 0.0501675, 0.0512835, 0.0523489, 0.0533601, 0.0543134, 0.0552053, 0.0560321, 0.0567903, 0.0574765, 0.0580873, 0.0586193, 0.0590696, 0.059435, 0.0597128, 0.0599003, 0.0599951, 0.0598982, 0.0597028, 0.0594075, 0.0590111, 0.0585128, 0.0579121, 0.0572087, 0.0564028, 0.0554948, 0.0544854, 0.0533756, 0.0521668, 0.0508605, 0.0494586, 0.0479633, 0.0463768, 0.0447018, 0.042941, 0.0410972, 0.0391735, 0.037173, 0.0350989, 0.0329544, 0.0307426, 0.0284669, 0.0261302, 0.0237357, 0.0212862, 0.0187844, 0.0162331, 0.0136345, 0.0109908, 0.008304, 0.0055757, 0.00280732, 0.0, -0.00280732, -0.0055757, -0.008304, -0.0109908, -0.0136345, -0.0162331, -0.0187844, -0.0212862, -0.0237357, -0.0261302, -0.0284669, -0.0307426, -0.0329544, -0.0350989, -0.037173, -0.0391735, -0.0410972, -0.042941, -0.0447018, -0.0463768, -0.0479633, -0.0494586, -0.0508605, -0.0521668, -0.0533756, -0.0544854, -0.0554948, -0.0564028, -0.0572087, -0.0579121, -0.0585128, -0.0590111, -0.0594075, -0.0597028, -0.0598982, -0.0599951, -0.0599003, -0.0597128, -0.059435, -0.0590696, -0.0586193, -0.0580873, -0.0574765, -0.0567903, -0.0560321, -0.0552053, -0.0543134, -0.0533601, -0.0523489, -0.0512835, -0.0501675, -0.0490045, -0.0477982, -0.0465521, -0.0452698, -0.0439549, -0.0426107, -0.0412407, -0.0398482, -0.0384366, -0.0370091, -0.035569, -0.0341194, -0.0326635, -0.0312044, -0.0297451, -0.0282887, -0.0268382, -0.0253966, -0.0239669, -0.0225521, -0.0211551, -0.0197789, -0.0184265, -0.0171006, -0.0158044, -0.0145406, -0.0133121, -0.0121219, -0.0109726, -0.00986703, -0.00880793, -0.00779793, -0.00683958, -0.00593537, -0.00508767, -0.00429874, -0.00357073, -0.00290565, -0.00230534, -0.00177151, -0.00130567, -0.00090917, -0.00058316, -0.000328595, -0.000146223, -3.65828e-05];
 
-foil_top = max(y)+5*dx_foil
-foil_bottom = min(y)-5*dx_foil
-foil_front = min(x)-5*dx_foil
-foil_back = max(x)+5*dx_foil
+foil_top = max(y)+8*dx_foil
+foil_bottom = min(y)-8*dx_foil
+foil_front = min(x)-8*dx_foil
+foil_back = max(x)+8*dx_foil
 
 # Transfinite (structured) meshing
 nx_foil = int((max(x)-min(x)) / dx_foil) + 1
 ny_foil = int((max(y)-min(y)) / dx_foil) + 1
-nx_around = int(Lx / dx_around) + 1
-ny_around = int(Lx / dx_around) + 1
+nx_in   = int(abs(xmin-foil_front) / dx_around) + 1
+nx_out  = int((xmax-foil_back) / dx_around) + 1
+ny_around = int((Ly-(foil_top-foil_bottom)) / dx_around / 2) + 1
 
 
 # Geometry points (as in your original setup)
@@ -62,19 +64,19 @@ def add_line(label, start, end, n, prog=False):
 
 # bottom left
 add_line("l1", "x0_y0", "x0_y1", ny_around, True)
-add_line("l2", "x0_y1", "x1_y1", nx_around, True)
+add_line("l2", "x0_y1", "x1_y1", nx_in, True)
 add_line("l3", "x1_y1", "x1_y0", ny_around, True)
-add_line("l4", "x1_y0", "x0_y0", nx_around, True)
+add_line("l4", "x1_y0", "x0_y0", nx_in, True)
 
 # mid left
 add_line("l5", "x0_y1", "x0_y2", ny_foil)
-add_line("l6", "x0_y2", "x1_y2", nx_around, True)
+add_line("l6", "x0_y2", "x1_y2", nx_in, True)
 add_line("l7", "x1_y2", "x1_y1", ny_foil)
-# reuse l2 instead of add_line("l8", "x1_y1", "x0_y1", nx_around, True)
+# reuse l2 instead of add_line("l8", "x1_y1", "x0_y1", nx_in, True)
 
 # top left
 add_line("l8", "x0_y2", "x0_y3", ny_around, True)
-add_line("l9", "x0_y3", "x1_y3", nx_around, True)
+add_line("l9", "x0_y3", "x1_y3", nx_in, True)
 add_line("l10", "x1_y3", "x1_y2", ny_around, True)
 # reuse l6 instead of re-adding "x1_y2" to "x0_y2"
 
@@ -97,19 +99,19 @@ add_line("l17", "x2_y3", "x2_y2", ny_around, True)
 # reuse l14: "x1_y2" to "x2_y2"
 
 # bottom right
-add_line("l18", "x2_y1", "x3_y1", nx_around, True)
+add_line("l18", "x2_y1", "x3_y1", nx_out, True)
 add_line("l19", "x3_y1", "x3_y0", ny_around, True)
-add_line("l20", "x3_y0", "x2_y0", nx_around, True)
+add_line("l20", "x3_y0", "x2_y0", nx_out, True)
 # reuse l12: "x2_y1" to "x2_y0"
 
 # mid right
-add_line("l21", "x2_y2", "x3_y2", nx_around, True)
+add_line("l21", "x2_y2", "x3_y2", nx_out, True)
 add_line("l22", "x3_y2", "x3_y1", ny_foil)
 # reuse l18: "x2_y1" to "x3_y1"
 # reuse l15: "x2_y2" to "x2_y1"
 
 # top right
-add_line("l23", "x2_y3", "x3_y3", nx_around, True)
+add_line("l23", "x2_y3", "x3_y3", nx_out, True)
 add_line("l24", "x3_y3", "x3_y2", ny_around, True)
 # reuse l21: "x2_y2" to "x3_y2"
 # reuse l17: "x2_y3" to "x2_y2"
@@ -151,18 +153,18 @@ def makeTransfiniteCurve(label, start, end, n, prog=0):
 
 # bottom left
 makeTransfiniteCurve("l1", "x0_y0", "x0_y1", ny_around, prog_y)
-makeTransfiniteCurve("l2", "x0_y1", "x1_y1", nx_around, prog_x)
+makeTransfiniteCurve("l2", "x0_y1", "x1_y1", nx_in, prog_x_in)
 makeTransfiniteCurve("l3", "x1_y1", "x1_y0", ny_around, -prog_y)
-makeTransfiniteCurve("l4", "x1_y0", "x0_y0", nx_around, -prog_x)
+makeTransfiniteCurve("l4", "x1_y0", "x0_y0", nx_in, -prog_x_in)
 
 # mid left
 makeTransfiniteCurve("l5", "x0_y1", "x0_y2", ny_foil)
-makeTransfiniteCurve("l6", "x0_y2", "x1_y2", nx_around, prog_x)
+makeTransfiniteCurve("l6", "x0_y2", "x1_y2", nx_in, prog_x_in)
 makeTransfiniteCurve("l7", "x1_y2", "x1_y1", ny_foil)
 
 # top left
 makeTransfiniteCurve("l8", "x0_y2", "x0_y3", ny_around, -prog_y)
-makeTransfiniteCurve("l9", "x0_y3", "x1_y3", nx_around, prog_x)
+makeTransfiniteCurve("l9", "x0_y3", "x1_y3", nx_in, prog_x_in)
 makeTransfiniteCurve("l10", "x1_y3", "x1_y2", ny_around, prog_y)
 
 # bottom mid
@@ -179,16 +181,16 @@ makeTransfiniteCurve("l16", "x1_y3", "x2_y3", nx_foil)
 makeTransfiniteCurve("l17", "x2_y3", "x2_y2", ny_around, prog_y)
 
 # bottom right
-makeTransfiniteCurve("l18", "x2_y1", "x3_y1", nx_around, -prog_x)
+makeTransfiniteCurve("l18", "x2_y1", "x3_y1", nx_out, -prog_x_out)
 makeTransfiniteCurve("l19", "x3_y1", "x3_y0", ny_around, -prog_y)
-makeTransfiniteCurve("l20", "x3_y0", "x2_y0", nx_around, prog_x)
+makeTransfiniteCurve("l20", "x3_y0", "x2_y0", nx_out, prog_x_out)
 
 # mid right
-makeTransfiniteCurve("l21", "x2_y2", "x3_y2", nx_around, -prog_x)
+makeTransfiniteCurve("l21", "x2_y2", "x3_y2", nx_out, -prog_x_out)
 makeTransfiniteCurve("l22", "x3_y2", "x3_y1", ny_foil)
 
 # top right
-makeTransfiniteCurve("l23", "x2_y3", "x3_y3", nx_around, -prog_x)
+makeTransfiniteCurve("l23", "x2_y3", "x3_y3", nx_out, -prog_x_out)
 makeTransfiniteCurve("l24", "x3_y3", "x3_y2", ny_around, prog_y)
 
 
@@ -241,55 +243,42 @@ mesh = meshio.read(tmp_msh)
 foil_points = [(x[i], y[i]) for i in range(len(x))]
 foil_polygon = shapely.geometry.Polygon(foil_points)
 
-
-# Extract points and cells
 points = mesh.points
-cells = mesh.cells_dict
+cells = mesh.cells
+cell_data = mesh.cell_data
 
-# Filter points with x >= 0 and y >= 0
-valid_indices = np.all(points[:, :2] >= 0, axis=1)
-valid_points = points[valid_indices]
+# Determine valid point indices (you may still keep all points if needed)
+valid_cells = []
+new_cell_data = {}
 
-# Create a mapping from old point indices to new point indices
-old_to_new_indices = np.full(len(points), -1, dtype=int)
-old_to_new_indices[valid_indices] = np.arange(np.sum(valid_indices))
+for cell_block_index, cell_block in enumerate(cells):
+    cell_type = cell_block.type
+    cell_array = cell_block.data
+    valid_cell_indices = []
 
-# Filter cells based on the valid points
-filtered_cells = {}
-for cell_type, cell_array in cells.items():
-    valid_cells = np.all(np.isin(cell_array, np.nonzero(valid_indices)[0]), axis=1)
-    filtered_cells[cell_type] = old_to_new_indices[cell_array[valid_cells]]
+    for i, element in enumerate(cell_array):
+        coords = points[element]
+        p = shapely.geometry.Point(np.mean(coords[:,0]), np.mean(coords[:,1]))
+        if not foil_polygon.contains(p):
+            valid_cell_indices.append(i)
 
-# Filter point_data
-filtered_point_data = {}
-for key, data in mesh.point_data.items():
-    filtered_point_data[key] = data[valid_indices]
+    # Filter cell data accordingly
+    valid_cell_array = cell_array[valid_cell_indices]
+    valid_cells.append((cell_type, valid_cell_array))
 
-# Filter cell_data
-filtered_cell_data = {}
-for key, data_dict in mesh.cell_data.items():
-    if isinstance(data_dict, dict):
-        filtered_cell_data[key] = {}
-        for cell_type, data in data_dict.items():
-            if cell_type in filtered_cells:
-                valid_cells = np.all(np.isin(cells[cell_type], np.nonzero(valid_indices)[0]), axis=1)
-                filtered_cell_data[key][cell_type] = data[valid_cells]
-    elif isinstance(data_dict, list):
-        filtered_cell_data[key] = []
-        for data in data_dict:
-            filtered_data = {}
-            for cell_type, cell_data in data:
-                if cell_type in filtered_cells:
-                    valid_cells = np.all(np.isin(cells[cell_type], np.nonzero(valid_indices)[0]), axis=1)
-                    filtered_data[cell_type] = cell_data[valid_cells]
-            filtered_cell_data[key].append(filtered_data)
+    # Handle cell data (if any)
+    for key in cell_data:
+        if key not in new_cell_data:
+            new_cell_data[key] = []
+        new_cell_data[key].append(cell_data[key][cell_block_index][valid_cell_indices])
 
-# Create a new mesh with the filtered points and cells
+# Convert back to meshio format
 filtered_mesh = meshio.Mesh(
-    points=valid_points,
-    cells=filtered_cells,
-    point_data=filtered_point_data,
-    cell_data=filtered_cell_data,
-    field_data=mesh.field_data
+    points=points,  # Retain all points, or optionally remove unused ones
+    cells=[meshio.CellBlock(cell_type, data) for cell_type, data in valid_cells],
+    point_data=mesh.point_data,
+    cell_data=new_cell_data,
+    field_data=mesh.field_data,
 )
+# filtered_mesh.write("output.msh")
 filtered_mesh.write("output2.msh", file_format="gmsh22")
